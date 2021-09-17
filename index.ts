fce3d87b0b7981279fc3b1442fb1239e8280f330
Alex Nicolas <alexoliveira604@gmail.com>
2021-09-17 17:13:38 -0300

    initial commit

---
+++ index.ts
@@ -0,0 +1,9 @@
+import { ApolloServer, gql } from "apollo-server";
+import { typeDefs } from "./types"; 
+import { resolvers } from "./resolvers"; 
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
---
+++ resolvers/index.ts
@@ -0,0 +1,20 @@
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
+
+export const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
