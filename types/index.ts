0a216be700ce223b332e4b70453f2049caa318c4
Alex Nicolas <alexoliveira604@gmail.com>
2021-09-17 18:19:35 -0300

    Book and Resolvers type was created

--- types/index.ts
+++ types/index.ts
@@ -4,9 +4,22 @@ export const typeDefs = gql`
   type Book {
     title: String
     author: String
+    description: String
   }
 
   type Query {
     books: [Book]
   }
 `;
+
+export type Book = {
+  title: string
+  author: string
+  description: string
+}
+
+export type Resolvers = {
+  Query: {
+    books: () => Book,
+  },
+}
