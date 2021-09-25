053b0c6ccacde42c52096da8c4a95c5a293a2c0b
AlexNicolasCode <alexoliveira604@gmail.com>
2021-09-25 15:10:53 -0300

    update types about book system

--- types/index.ts
+++ types/index.ts
@@ -2,24 +2,20 @@ import { gql } from "apollo-server";
 
 export const typeDefs = gql`
   type Book {
+    id: String
     title: String
     author: String
     description: String
   }
 
+  type Mutation {
+    addBook(title: String, author: String, description: String): Book
+    updateBook(id: String, newTitle: String, newAuthor: String, newDescription: String): Book
+    deleteBook(id: String): Book
+  }
+
   type Query {
     books: [Book]
+    getBook(id: String): Book
   }
 `;
-
-export type Book = {
-  title: string
-  author: string
-  description: string
-}
-
-export type Resolvers = {
-  Query: {
-    books: () => Book,
-  },
-}
