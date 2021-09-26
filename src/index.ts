c56b8713dd23698c330cc2aa3fc994c1bd080918
Alex Nicolas <alexoliveira604@gmail.com>
2021-09-26 13:34:02 -0300

    deploy

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,6 @@ mongoose.connect(process.env.URI ?? uri);
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen().then(({ url }) => {
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
