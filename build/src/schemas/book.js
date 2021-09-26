82598ef792b3b5bcc93f229efb089a83ca2902b9
AlexNicolasCode <alexoliveira604@gmail.com>
2021-09-26 14:59:18 -0300

    change env

---
+++ build/src/schemas/book.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+exports.Book = void 0;
+var mongoose_1 = require("mongoose");
+var BookSchema = new mongoose_1.Schema({
+    title: String,
+    author: String,
+    description: String
+});
+exports.Book = (0, mongoose_1.model)("books", BookSchema);
