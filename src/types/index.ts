247e58089beede2a63fe4d06ad7c62f5333ca82e
AlexNicolasCode <alexoliveira604@gmail.com>
2021-10-03 10:23:04 -0300

    user can to add token when send data

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,21 +1,35 @@
 import { gql } from "apollo-server";
 
 export const typeDefs = gql`
+  type User {
+    name: String,
+    email: String,
+  }
+
   type Book {
     id: String
     title: String
     author: String
     description: String
+    createdBy: String
+  }
+
+  type Token {
+    token: String
   }
 
   type Mutation {
+    signUpUser(name: String, email: String, password: String): Token
     addBook(title: String, author: String, description: String): Book
     updateBook(id: String, newTitle: String, newAuthor: String, newDescription: String): Book
+    updateNotes(id: String, newTitle: String, newAuthor: String, newDescription: String, newNotes): Book
     deleteBook(id: String): Book
   }
 
   type Query {
-    books: [Book]
+    getAllBooks: [Book]
     getBook(id: String): Book
+    loginUser(name: String, email: String, password: String): Token
+    autoLogin: User
   }
 `;
