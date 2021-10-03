efb6f48926ca4e7e6d5d2d9a9e91cd0d6acd73b1
AlexNicolasCode <alexoliveira604@gmail.com>
2021-10-03 14:55:22 -0300

    update password decoder

--- src/user/tools/validadeUser.ts
+++ src/user/tools/validadeUser.ts
@@ -1,14 +1,12 @@
-const jwt = require("jsonwebtoken");
-
-const key = "test";
+import * as jwt from "jsonwebtoken";
 
 export const createToken = async (name, email) => {
 	const userProps = { name: name, email: email };
-	const token = jwt.sign(userProps, key, { expiresIn: 60 * 60 * 60 })
+	const token = jwt.sign(userProps, process.env.KEY, { expiresIn: 60 * 60 * 60 })
 
 	return { token: token }
 } 
 
 export const verifyToken = (token) => {
-	return jwt.verify(token, key)
+	return jwt.verify(token, process.env.KEY)
 }
