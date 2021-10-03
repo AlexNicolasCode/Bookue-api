8c833866a3234619c48799b9a80541282f6a2564
AlexNicolasCode <alexoliveira604@gmail.com>
2021-10-03 14:22:59 -0300

    deploy allow

---
+++ build/src/resolver/index.js
@@ -0,0 +1,53 @@
+"use strict";
+exports.__esModule = true;
+exports.resolvers = void 0;
+var addBook_1 = require("../book/resolvers/addBook");
+var deleteBook_1 = require("../book/resolvers/deleteBook");
+var getAllBooks_1 = require("../book/resolvers/getAllBooks");
+var getBook_1 = require("../book/resolvers/getBook");
+var updateBook_1 = require("../book/resolvers/updateBook");
+var loginResolver_1 = require("../user/resolvers/loginResolver");
+var signUpResolver_1 = require("../user/resolvers/signUpResolver");
+var autoLoginResolver_1 = require("../user/resolvers/autoLoginResolver");
+exports.resolvers = {
+    Query: {
+        getAllBooks: function (_, __, _a) {
+            var token = _a.token;
+            return (0, getAllBooks_1.getAllBooks)(token);
+        },
+        getBook: function (_, _a, _b) {
+            var id = _a.id;
+            var token = _b.token;
+            return (0, getBook_1.getBook)(id, token);
+        },
+        loginUser: function (_, _a) {
+            var name = _a.name, email = _a.email, password = _a.password;
+            return (0, loginResolver_1.loginUser)(name, email, password);
+        },
+        autoLogin: function (_, __, _a) {
+            var token = _a.token;
+            return (0, autoLoginResolver_1.autoLogin)(token);
+        }
+    },
+    Mutation: {
+        signUpUser: function (_, _a) {
+            var name = _a.name, email = _a.email, password = _a.password;
+            return (0, signUpResolver_1.signUpUser)(name, email, password);
+        },
+        addBook: function (_, _a, _b) {
+            var title = _a.title, author = _a.author, description = _a.description;
+            var token = _b.token;
+            return (0, addBook_1.addBook)(token, title, author, description);
+        },
+        updateBook: function (_, _a, _b) {
+            var id = _a.id, newTitle = _a.newTitle, newAuthor = _a.newAuthor, newDescription = _a.newDescription;
+            var token = _b.token;
+            return (0, updateBook_1.updateBook)(token, id, newTitle, newAuthor, newDescription);
+        },
+        deleteBook: function (_, _a, _b) {
+            var id = _a.id;
+            var token = _b.token;
+            return (0, deleteBook_1.deleteBook)(token, id);
+        }
+    }
+};
---
+++ build/src/types/index.js
@@ -0,0 +1,10 @@
+"use strict";
+var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
+    if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
+    return cooked;
+};
+exports.__esModule = true;
+exports.typeDefs = void 0;
+var apollo_server_1 = require("apollo-server");
+exports.typeDefs = (0, apollo_server_1.gql)(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  type User {\n    name: String,\n    email: String,\n  }\n\n  type Book {\n    id: String\n    title: String\n    author: String\n    description: String\n    createdBy: String\n  }\n\n  type Token {\n    token: String\n  }\n\n  type Mutation {\n    signUpUser(name: String, email: String, password: String): Token\n    addBook(title: String, author: String, description: String): Book\n    updateBook(id: String, newTitle: String, newAuthor: String, newDescription: String): Book\n    deleteBook(id: String): Book\n  }\n\n  type Query {\n    getAllBooks: [Book]\n    getBook(id: String): Book\n    loginUser(name: String, email: String, password: String): Token\n    autoLogin: User\n  }\n"], ["\n  type User {\n    name: String,\n    email: String,\n  }\n\n  type Book {\n    id: String\n    title: String\n    author: String\n    description: String\n    createdBy: String\n  }\n\n  type Token {\n    token: String\n  }\n\n  type Mutation {\n    signUpUser(name: String, email: String, password: String): Token\n    addBook(title: String, author: String, description: String): Book\n    updateBook(id: String, newTitle: String, newAuthor: String, newDescription: String): Book\n    deleteBook(id: String): Book\n  }\n\n  type Query {\n    getAllBooks: [Book]\n    getBook(id: String): Book\n    loginUser(name: String, email: String, password: String): Token\n    autoLogin: User\n  }\n"])));
+var templateObject_1;
