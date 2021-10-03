4255efebe0dc9b49d37484bb4ff6cfad1b2d48b0
Alex Nicolas <alexoliveira604@gmail.com>
2021-10-03 10:16:52 -0300

    path update

--- src/schemas/book.ts
+++
@@ -1,9 +0,0 @@
-import { model, Schema } from "mongoose";
-
-const BookSchema = new Schema({
-    title: String,
-    author: String,
-    description: String,
-})
-
-export const Book = model("books", BookSchema);
