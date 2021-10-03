7d0c63141ac7aaac25856ce8900aa855f0d133ce
AlexNicolasCode <alexoliveira604@gmail.com>
2021-10-03 10:16:02 -0300

    user can to send the authorization token in headers

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
 import * as mongoose from "mongoose";
-import { ApolloServer, gql } from "apollo-server";
+import { ApolloServer } from "apollo-server";
 import { typeDefs } from "./types"; 
-import { resolvers } from "./resolvers"; 
+import { resolvers } from "./resolver"; 
+import uri from "../env"
 
-mongoose.connect(process.env.URI);
+mongoose.connect(process.env.URI || uri);
 !mongoose.connection ? console.log("Error connecting db") : console.log("Db connected successfully")
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ 
+  typeDefs, 
+  resolvers, 
+  context: ({ req }) => {
+    const token = req.headers.authorization || '';
+    return { token }
+  }
+});
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
