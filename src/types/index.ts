0e1d75448eec7fbefc7df94cab848a91edd950ee
Alex Nicolas <alexoliveira604@gmail.com>
2021-10-23 14:35:52 -0300

    fix: front can receive notes from database

--- src/types/index.ts
+++ src/types/index.ts
@@ -22,7 +22,7 @@ export const typeDefs = gql`
     signUpUser(name: String, email: String, password: String): Token
     addBook(title: String, author: String, description: String): Book
     updateBook(id: String, newTitle: String, newAuthor: String, newDescription: String): Book
-    updateNotes(id: String, newTitle: String, newAuthor: String, newDescription: String, newNotes): Book
+    updateNotes(id: String, newTitle: String, newAuthor: String, newDescription: String, newNotes: String): Book
     deleteBook(id: String): Book
   }
 
