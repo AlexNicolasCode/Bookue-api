73fe6d3ef3c77a64291e627266cee9d8bb8e4dda
Alex Nicolas <alexoliveira604@gmail.com>
2021-11-06 19:41:45 -0300

    fix: user must able to add a new note

--- src/note/resolvers/addNote.ts
+++ src/note/resolvers/addNote.ts
@@ -3,27 +3,24 @@ import { findBook } from "../tools/findBook";
 import { updateNotes } from "../tools/updateNotes";
 
 export const addNote = async (token, bookID, note) => {
-  const user: any = await verifyToken(token)
-  let newNotes;
+    const user: any = await verifyToken(token)
 
-  if (!user) {
+    if (!user) {
     return
-  }
+    }
 
-  const book = await findBook(bookID, user.email);
-  if (book && book.notes[0] && isValidNote(book.notes, note)) {
-    newNotes = await setNewNotes(book.notes, note);
-    await updateNotes(bookID, newNotes)
+    let book = await findBook(bookID, user.email);
+    if (book.notes[0]) {
+        book.notes = []
+    }
+
+    const newNotes = await setNewNotes(book.notes, note);
+    
+    if (isValidNote(book.notes, newNotes)) {
+        await updateNotes(bookID, newNotes)
+    }
+
     return await isAdded(bookID, user.email, newNotes)
-  }
-
-  newNotes = {
-    notes: [
-        { text: note, created_at: Date.now() }
-    ],
-  }
-  await updateNotes(bookID, newNotes)
-  return await isAdded(bookID, user.email, newNotes)
 } 
 
 const isAdded = async (bookID, email, newNotes) => {
