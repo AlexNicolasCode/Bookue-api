b3e7e55dae6d25266098f3614e6a57198662f581
Alex Nicolas <alexoliveira604@gmail.com>
2021-12-01 22:40:27 -0300

    fix: password hash must be correctly generated

--- src/user/tools/passwordEncoder.ts
+++ src/user/tools/passwordEncoder.ts
@@ -1,5 +1,5 @@
-import { createHmac } from "crypto";
+import * as crypto from 'crypto';
 
-export const hashPassword = (password: string) => {
-	return createHmac("sha256", process.env.KEY_PASSWORD).update(password).digest('hex');
-} 
+export const hashPassword = async (password: string): Promise<string> => {
+    return crypto.pbkdf2Sync(password, process.env.KEY, 1000, 64, `sha512`).toString(`hex`);
+}
