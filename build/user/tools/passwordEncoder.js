814a4ca1756f363060e6cabb3ffb5c08087b0c1c
Alex Nicolas <alexoliveira604@gmail.com>
2021-12-01 21:51:28 -0300

    feat: CI must create build version

--- build/user/tools/passwordEncoder.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.hashPassword = void 0;
-var crypto_1 = require("crypto");
-var hashPassword = function (password) {
-    return (0, crypto_1.createHash)('sha256').update("" + password + process.env.PASSWORD_KEY).digest('hex');
-};
-exports.hashPassword = hashPassword;
