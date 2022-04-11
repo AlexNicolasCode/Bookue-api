0707d2b3f107369884e01b31df9c465c4f3481bc
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-10 21:51:21 -0300

    feat: BookResultModel of domain was created

---
+++ src/domain/models/book.result.ts
@@ -0,0 +1,9 @@
+export type BookResultModel = {
+    title: String,
+    author: String,
+    description: String,
+    currentPage: String,
+    pages: String,
+    created_by: String,
+    created_at: Date,
+}
