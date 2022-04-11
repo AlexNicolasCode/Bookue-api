4bb2c26e10d5a2e50ab3e2945d850a3f7e2636eb
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-11 02:21:26 -0300

    feat: MongoHelper should able to connet add new data on database

--- src/infra/db/mongo.helper.ts
+++ src/infra/db/mongo.helper.ts
@@ -1,3 +1,5 @@
+import { Book } from "@/book/schema/book";
+import { User } from "@/user/schema/user";
 import * as mongoose from "mongoose";
 
 export const MongoHelper = {
@@ -11,4 +13,33 @@ export const MongoHelper = {
     async checkConnection (): Promise<void> {
         !mongoose.connection ? console.log("Error connecting db") : console.log("Db connected successfully")
     },
+    
+    async addOne (colletion: mongoose.Schema, data): Promise<void> {
+        try {
+            const schema = await this.findSchema(colletion);
+            const model = await this.findModel('books', schema);
+            await model.create(data);
+        } catch (e) {
+            throw new Error(e);
+        }
+    },
+
+    async findSchema (schemaName: string): Promise<mongoose.Schema> {
+        try {
+            const schemaMapper = {
+                'book': Book,
+                'user': User,
+            };
+            return schemaMapper[schemaName];
+        } catch (e) {
+            throw new Error(e);
+        };
+    },
+
+    async findModel (modelName: string, schema: mongoose.Schema ): Promise<any> {
+        const model = mongoose.model(modelName, schema);
+        return model;
+    },
+
+    
 }
