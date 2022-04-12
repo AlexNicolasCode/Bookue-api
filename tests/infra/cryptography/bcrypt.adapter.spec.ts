0cdcc9b9e964b912ec678dd3759715e02b35d873
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-12 08:59:23 -0300

    test: BcryptAdapter should return a valid hash on hash success

--- tests/infra/cryptography/bcrypt.adapter.spec.ts
+++ tests/infra/cryptography/bcrypt.adapter.spec.ts
@@ -2,6 +2,20 @@ import { Hasher } from "@/data/protocols";
 
 import * as bcrypt from 'bcrypt'
 
+type SutTypes = {
+    sut: BcryptAdapter,
+    salt: number
+}
+
+const makeSut = (): SutTypes => {
+    const salt = 12
+    const sut = new BcryptAdapter(salt)
+    return {
+        sut,
+        salt
+    }
+}
+
 class BcryptAdapter implements Hasher {
     constructor (private readonly salt: number) {}
 
@@ -18,12 +32,19 @@ jest.mock('bcrypt', () => ({
 
 describe('BcryptAdapter', () => {
     test('should call hash with correct values', async () => {
-        const salt = 12
-        const sut = new BcryptAdapter(salt)
+        const { sut, salt } = makeSut()
         const hashSpy = jest.spyOn(bcrypt, 'hash')
         
         await sut.hash('any_value')
 
         expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+    })
+
+    test('should return a valid hash on hash success', async () => {
+        const { sut } = makeSut()
+           
+        const hash = await sut.hash('any_value')
+
+        expect(hash).toBe('hash')
     })
 })
