2c1b68caf32fb072a217d84cdd3b9343e702b527
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-12 07:48:32 -0300

    feat: CheckAccountByEmailRepositorySpy should return false by default

--- tests/data/mocks/mock.account.repository.ts
+++ tests/data/mocks/mock.account.repository.ts
@@ -2,7 +2,7 @@ import { CheckAccountByEmailRepository } from "@/data/protocols";
 
 export class CheckAccountByEmailRepositorySpy implements CheckAccountByEmailRepository {
     email: string
-    result = true
+    result = false
 
     async checkByEmail (email: string): Promise<CheckAccountByEmailRepository.Result> {
         this.email = email
