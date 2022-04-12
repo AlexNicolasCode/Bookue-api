6920e88753420c3cc5b0587d6618766eed2317a7
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-12 07:49:31 -0300

    test: DbAddAccount should pass correct param to Hasher

--- tests/data/usecases/db.add.account.repository.spec.ts
+++ tests/data/usecases/db.add.account.repository.spec.ts
@@ -1,27 +1,56 @@
-import { CheckAccountByEmailRepository } from "@/data/protocols";
+import { CheckAccountByEmailRepository, Hasher } from "@/data/protocols";
 import { AddAccount } from "@/domain/usecases";
 import { mockAddAccountParams } from "tests/domain/mocks";
-import { CheckAccountByEmailRepositorySpy } from "../mocks";
+import { CheckAccountByEmailRepositorySpy, HasherSpy } from "../mocks";
+
+type SutTypes = {
+    sut: DbAddAccount
+    hasher: HasherSpy
+    checkAccountByEmailRepository: CheckAccountByEmailRepositorySpy
+}
+
+const makeSut = (): SutTypes => {
+    const hasher = new HasherSpy()
+    const checkAccountByEmailRepository = new CheckAccountByEmailRepositorySpy()
+    const sut = new DbAddAccount(hasher, checkAccountByEmailRepository)
+    return {
+        sut,
+        hasher,
+        checkAccountByEmailRepository
+    }
+} 
 
 class DbAddAccount implements AddAccount {
     constructor (
+        private readonly hasher: Hasher,
         private readonly checkAccountByEmailRepository: CheckAccountByEmailRepository
-    ) {}
-
-    async add (accountData: AddAccount.Params): Promise<AddAccount.Result> {
-        const exists = await this.checkAccountByEmailRepository.checkByEmail(accountData.email) 
-        return true
+        ) {}
+        
+        async add (accountData: AddAccount.Params): Promise<AddAccount.Result> {
+            const hasAccount = await this.checkAccountByEmailRepository.checkByEmail(accountData.email) 
+            if (!hasAccount) {
+                await this.hasher.hash(accountData.password)
+                return true
+            }
+        }
     }
-}
 
 describe('DbAddAccount', () => {
     test('should pass correct param to checkAccountByEmailRepository', async () => {
-        const checkAccountByEmailRepository = new CheckAccountByEmailRepositorySpy()
+        const { sut, checkAccountByEmailRepository } = makeSut()  
         const addAccountParams = mockAddAccountParams()
-        const sut = new DbAddAccount(checkAccountByEmailRepository)
 
         await sut.add(addAccountParams)
 
         expect(addAccountParams.email).toBe(checkAccountByEmailRepository.email)
+    })
+
+    test('should pass correct param to Hasher', async () => {
+        const { sut, hasher } = makeSut()  
+        const addAccountParams = mockAddAccountParams()
+
+        await sut.add(addAccountParams)
+
+        expect(addAccountParams.password).toBe(hasher.plaintext)
     })
 })
