7c1cb3f2990ed141b2bdb1781bef5d3254f016eb
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-11 23:26:08 -0300

    feat: BookMongoRepository was created

--- tests/infra/db/book.mongo.repository.spec.ts
+++ tests/infra/db/book.mongo.repository.spec.ts
@@ -1,19 +1,8 @@
-import { AddBookRepository } from "@/data/protocols";
-import { BookModel } from "@/domain/models";
+import { BookMongoRepository } from "@/infra/db/book.mongo.repository";
 import { MongoHelper } from "@/infra";
 import { mockBookModel } from "tests/domain/mocks";
 
 import env from '@/env'
-
-class BookMongoRepository implements AddBookRepository {
-    async add (bookData: BookModel): Promise<void> {
-        try {
-            await MongoHelper.addOneOn('book', bookData);
-        } catch (e) {
-            throw new Error(e);
-        }
-    }
-}
 
 describe('BookMongoRepository', () => {
     beforeAll(async () => {
