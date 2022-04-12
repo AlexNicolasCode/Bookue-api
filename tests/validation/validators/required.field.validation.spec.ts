adc8ddb819d89d25beb1b382e7e316a55f2063ee
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-12 00:13:27 -0300

    test: RequiredFieldValidation should not return on success

--- tests/validation/validators/required.field.validation.spec.ts
+++ tests/validation/validators/required.field.validation.spec.ts
@@ -5,12 +5,29 @@ import faker from "@faker-js/faker";
 
 const field = faker.random.word();
 
+type SutTypes = {
+    sut: RequiredFieldValidation
+}
+
+const makeSut = (): SutTypes => {
+    const sut = new RequiredFieldValidation(field);
+    return { sut }
+}
+
 describe('RequiredFieldValidation', () => {
     test('should return a MissingParamError when a field is missing', () => {
-        const sut = new RequiredFieldValidation(field);
-
+        const { sut } = makeSut();
+        
         const error = sut.validate({ invalidField: faker.random.word() })
-
+        
         expect(error).toEqual(new MissingParamError(field))
-    })
+    });
+    
+    test('should not return on success', () => {
+        const { sut } = makeSut();
+
+        const error = sut.validate({ [field]: faker.random.word() })
+
+        expect(error).toBeUndefined()
+    });
 })
