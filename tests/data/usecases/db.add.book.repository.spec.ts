41644fbde8b67a6ca13410907873d0d90eb793c6
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-11 23:35:33 -0300

    refactor: initial sut setup

--- tests/data/usecases/db.add.book.repository.spec.ts
+++ tests/data/usecases/db.add.book.repository.spec.ts
@@ -1,15 +1,29 @@
 import { DbAddBook } from "@/data/usecases";
 import { mockBookModel } from "tests/domain/mocks";
+import { throwError } from "tests/domain/mocks/test.helpers";
 import { AddBookRepositorySpy } from "../mocks";
+
+type SutTypes = {
+    sut: DbAddBook
+    addBookRepositorySpy: AddBookRepositorySpy
+}
+
+const makeSut = (): SutTypes => {
+    const addBookRepositorySpy = new AddBookRepositorySpy();
+    const sut = new DbAddBook(addBookRepositorySpy);
+    return {
+        sut,
+        addBookRepositorySpy
+    }
+}
 
 describe('DbAddBook', () => {
     test('should send correct bookData', () => {
-        const addBookRepositorySpy = new AddBookRepositorySpy();
-        const sut = new DbAddBook(addBookRepositorySpy);
-        const data = mockBookModel();
-
-        sut.add(data);
-
-        expect(addBookRepositorySpy.bookData).toBe(data);
-    })
+        const { sut, addBookRepositorySpy } = makeSut();
+        const bookData = mockBookModel();
+        
+        sut.add(bookData);
+        
+        expect(addBookRepositorySpy.bookData).toBe(bookData);
+    });
 })
