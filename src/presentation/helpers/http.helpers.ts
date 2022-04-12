e178d77f714d4e4f6d4512199a959642163d21cb
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-12 01:19:30 -0300

    feat: serverError http helper was created

--- src/presentation/helpers/http.helpers.ts
+++ src/presentation/helpers/http.helpers.ts
@@ -1,6 +1,12 @@
+import { ServerError } from "../errors";
 import { HttpReponse } from "../protocols";
 
 export const badRequest = (error: Error): HttpReponse  => ({
     statusCode: 400,
     body: error
+})
+
+export const serverError = (error: Error): HttpReponse  => ({
+    statusCode: 500,
+    body: new ServerError(error.stack)
 })
