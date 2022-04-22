008a57ed72a569eef24927c9e605cb5d52ee9873
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-22 19:33:28 -0300

    test: SignUpController should return 400 if Validation returns an error

---
+++ tests/presentation/controllers/signup.controller.spec.ts
@@ -0,0 +1,52 @@
+import { MissingParamError } from "@/presentation/errors";
+import { badRequest } from "@/presentation/helpers";
+import { Controller, HttpReponse, Validation } from "@/presentation/protocols";
+import faker from "@faker-js/faker";
+import { throwError } from "tests/domain/mocks/test.helpers";
+import { ValidationSpy } from "../mocks";
+
+export class SignUpController implements Controller {
+    constructor (
+        private validation: Validation,
+    ) {}
+
+    async handle (request: any): Promise<HttpReponse> {
+        try {
+            const error = await this.validation.validate(request)
+            if (error) {
+                return badRequest(error)
+            }
+        } catch (e) {}
+    }
+}
+
+const mockRequest = (): SignUpController.Request => {
+    const password = faker.internet.password()
+    return {
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      password,
+      passwordConfirmation: password
+    }
+}
+
+namespace SignUpController {
+    export type Request = {
+        name: string
+        email: string
+        password: string
+        passwordConfirmation: string
+    }
+}
+
+describe('SignUpController', () => {
+    test('should return 400 if Validation returns an error', async () => {
+        const validationSpy = new ValidationSpy()
+        const sut = new SignUpController(validationSpy)
+        validationSpy.error = new MissingParamError(faker.random.word())
+
+        const httpResponse = await sut.handle(mockRequest())
+
+        expect(httpResponse).toStrictEqual(badRequest(validationSpy.error))
+    })
+})
