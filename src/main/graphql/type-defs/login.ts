944cf0f937954952984eb305a18dfc9a103f8da8
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-22 20:33:45 -0300

    feat: sign up graphql type defs was defined

---
+++ src/main/graphql/type-defs/login.ts
@@ -0,0 +1,12 @@
+import { gql } from "apollo-server-express";
+
+export default gql`
+    type Mutation {
+        signUp (name: String!, email: String!, password: String!, passwordConfirmation: String!): Account!
+    }
+
+    type Account {
+        accessToken: String!
+        name: String!
+    }
+`
