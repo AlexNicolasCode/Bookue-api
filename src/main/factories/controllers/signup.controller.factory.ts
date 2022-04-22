e6247f4da812ce63073ec8e59cdbc5128d729ab8
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-22 20:26:04 -0300

    feat: makeSignUpController factory was created

---
+++ src/main/factories/controllers/signup.controller.factory.ts
@@ -0,0 +1,9 @@
+import { SignUpController } from "@/presentation/controllers";
+import { makeDbAddAccount } from "../usecases";
+import { makeDbAuthentication } from "../usecases/authentication.factory";
+import { makeSignUpValidation } from "./signup.validation.factory";
+
+export const makeSignUpController = (): SignUpController => {
+    const controller = new SignUpController(makeDbAddAccount(), makeSignUpValidation(), makeDbAuthentication())
+    return controller
+}
