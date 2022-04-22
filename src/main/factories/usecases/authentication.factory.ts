638cfd89ffef08cb5857f7594f6a0f690c2331fe
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-22 18:45:51 -0300

    feat: makeDbAuthentication factory was created

---
+++ src/main/factories/usecases/authentication.factory.ts
@@ -0,0 +1,13 @@
+import { DbAuthentication } from "@/data/usecases";
+import { Authentication } from "@/domain/usecases";
+import { AccountMongoRepository, BcryptAdapter } from "@/infra";
+import { JwtAdapter } from "@/infra/cryptography/jwt.adapter";
+import env from "@/main/config/env";
+
+export const makeDbAuthentication = (): Authentication => {
+    const salt = 12
+    const bcryptAdapter = new BcryptAdapter(salt)
+    const jwtAdapter = new JwtAdapter(env.jwtSecret)
+    const accountMongoRepository = new AccountMongoRepository()
+    return new DbAuthentication(accountMongoRepository, bcryptAdapter, jwtAdapter, accountMongoRepository)
+} 
