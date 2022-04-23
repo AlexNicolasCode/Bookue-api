cb4d31470c48f408db1f18ffac214118799612bb
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-23 13:19:15 -0300

    test: LoginController should return 401 when user is unauthorized

--- tests/presentation/controllers/login.controller.spec.ts
+++ tests/presentation/controllers/login.controller.spec.ts
@@ -1,19 +1,25 @@
+import { Authentication } from "@/domain/usecases";
 import { MissingParamError } from "@/presentation/errors";
-import { badRequest } from "@/presentation/helpers";
-import { Controller, HttpReponse, Validation } from "@/presentation/protocols";
+import { badRequest, unauthorized } from "@/presentation/helpers";
+import { Controller, HttpResponse, Validation } from "@/presentation/protocols";
 import faker from "@faker-js/faker";
-import { ValidationSpy } from "../mocks";
+import { AuthenticationSpy, ValidationSpy } from "../mocks";
 
 export class LoginController implements Controller {
     constructor (
         private readonly validation: Validation,
+        private readonly authentication: Authentication,
     ) {}
 
-    async handle (request: any): Promise<HttpReponse> {
+    async handle (request: any): Promise<HttpResponse> {
         try {
             const error = this.validation.validate(request)
             if (error) {
                 return badRequest(error)
+            }
+            const authenticationModel = await this.authentication.auth(request)
+            if (!authenticationModel) {
+                return unauthorized()
             }
         } catch (error) {}
     }
@@ -35,13 +41,26 @@ namespace SignUpController {
 
 describe('LoginController', () => {
     test('should return 400 when validation fails', async () => {
+        const authenticationSpy = new AuthenticationSpy()
         const validationSpy = new ValidationSpy()
-        const sut = new LoginController(validationSpy)
+        const sut = new LoginController(validationSpy, authenticationSpy)
         const fakeRequest = mockRequest()
         validationSpy.error = new MissingParamError(faker.random.word())
 
         const httpResponse = await sut.handle(fakeRequest)
 
         expect(httpResponse).toStrictEqual(badRequest(validationSpy.error))
+    })
+
+    test('should return 401 when user is unauthorized', async () => {
+        const authenticationSpy = new AuthenticationSpy()
+        const validationSpy = new ValidationSpy()
+        const sut = new LoginController(validationSpy, authenticationSpy)
+        const fakeRequest = mockRequest()
+        authenticationSpy.result = null
+
+        const httpResponse = await sut.handle(fakeRequest)
+
+        expect(httpResponse).toStrictEqual(unauthorized())
     })
 })
