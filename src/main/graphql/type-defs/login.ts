b090c69cfe3be4800f854d82f2f62d0e0ace9c03
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-23 13:43:16 -0300

    feat: login type defs was created

--- src/main/graphql/type-defs/login.ts
+++ src/main/graphql/type-defs/login.ts
@@ -1,6 +1,10 @@
 import { gql } from "apollo-server-express";
 
 export default gql`
+    type Query {
+        login (email: String!, password: String!): Account!
+    }
+
     type Mutation {
         signUp (name: String!, email: String!, password: String!, passwordConfirmation: String!): Account!
     }
