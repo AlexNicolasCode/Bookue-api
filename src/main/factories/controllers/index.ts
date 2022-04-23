a1d550b8699b7c4e89e523afdbe56a8b8d926741
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-23 13:38:28 -0300

    feat: makeLoginController was created

--- src/main/factories/controllers/index.ts
+++ src/main/factories/controllers/index.ts
@@ -3,3 +3,4 @@ export * from './add.book.controller.factory';
 export * from './signup.validation.factory';
 export * from './signup.controller.factory';
 export * from './login.validation.factory';
+export * from './login.controller.factory';
