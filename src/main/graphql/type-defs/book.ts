9a1ab3cbdb151f08ec2197d1c876e504e158c127
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-23 14:18:49 -0300

    feat: descriptions and current page shouldn't be required to add a new book

--- src/main/graphql/type-defs/book.ts
+++ src/main/graphql/type-defs/book.ts
@@ -2,7 +2,7 @@ import { gql } from "apollo-server-express";
 
 export default gql`
     type Mutation {
-        addBook (title: String!, author: String!, description: String!, currentPage: String!, pages: String!, userId: String!): Book!
+        addBook (title: String!, author: String!, description: String, currentPage: String, pages: String!, userId: String!): Book!
     }
 
     type Book {
