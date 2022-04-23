5a3989c0dbbd0b880267c56979ade23cede431ba
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-22 21:47:50 -0300

    feat: login validation factory should be imported using default index file

--- src/main/factories/controllers/index.ts
+++ src/main/factories/controllers/index.ts
@@ -2,3 +2,4 @@ export * from './add.book.validation.factory';
 export * from './add.book.controller.factory';
 export * from './signup.validation.factory';
 export * from './signup.controller.factory';
+export * from './login.validation.factory';
