1fcb7929092e1a04488b92dee583395feb1e514b
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-24 14:49:42 -0300

    test: LoadBookListController should return 500 if LoadBookList throws

---
+++ tests/presentation/controllers/load.book.list.controller.spec.ts
@@ -0,0 +1,39 @@
+import { LoadBookList } from "@/domain/usecases";
+import { serverError } from "@/presentation/helpers";
+import { Controller, HttpResponse } from "@/presentation/protocols";
+import { LoadBookListSpy } from "../mocks";
+import { throwError } from "tests/domain/mocks/test.helpers";
+
+import faker from "@faker-js/faker";
+
+class LoadBookListController implements Controller {
+    constructor (private readonly loadBookList: LoadBookList) {}
+
+    async handle (request: LoadBookListController.Request): Promise<HttpResponse> {
+        try {
+            await this.loadBookList.load(request.userId)
+            return
+        } catch (error) {
+            return serverError(error)
+        }
+    }
+}
+
+namespace LoadBookListController {
+    export type Request = {
+        userId: string
+    }
+}
+
+describe('LoadBookListController', () => {
+    test('should return 500 if LoadBookList throws', async () => {
+        const loadBookListSpy = new LoadBookListSpy()
+        const sut = new LoadBookListController(loadBookListSpy)
+        const fakeUserId = faker.datatype.uuid()
+        jest.spyOn(loadBookListSpy, 'load').mockImplementationOnce(throwError)
+
+        const httpResponse = await sut.handle({ userId: fakeUserId })
+
+        expect(httpResponse.statusCode).toStrictEqual(500)
+    })
+})
