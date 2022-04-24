6f686df465fb5d5fbe502eb98558bd348c9947f4
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-24 12:27:22 -0300

    test: PageCountValidation should return InvalidPageCountError if currentPage is more than pages

--- tests/validation/validators/pages.count.validation.spec.ts
+++ tests/validation/validators/pages.count.validation.spec.ts
@@ -13,11 +13,19 @@ class PageCountValidation implements Validation {
 }
 
 describe('PageCountValidation', () => {
-    test('should return undefined if pages is major than or or equal current', async () => {
+    test('should return undefined if pages is more than or or equal currentPage', async () => {
         const sut = new PageCountValidation()
         
         const result = sut.validate({ pages: 100, currentPage: 10 })
 
         expect(result).toBeUndefined()
+    })
+
+    test('should return InvalidPageCountError if currentPage is more than pages', async () => {
+        const sut = new PageCountValidation()
+        
+        const result = sut.validate({ pages: 10, currentPage: 100 })
+
+        expect(result).toStrictEqual(new InvalidPageCountError())
     })
 })
