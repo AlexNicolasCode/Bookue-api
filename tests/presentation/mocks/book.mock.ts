a03ed590447bcae37acc0050175e6896fab0fe71
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-24 14:42:00 -0300

    feat: LoadBookList was mocked

--- tests/presentation/mocks/book.mock.ts
+++ tests/presentation/mocks/book.mock.ts
@@ -1,9 +1,23 @@
-import { AddBook } from "@/domain/usecases";
+import { AddBook, LoadBookList } from "@/domain/usecases";
+import { mockBookModel } from "tests/domain/mocks";
 
 export class AddBookSpy implements AddBook {
     params: AddBook.Params
 
     async add (params: AddBook.Params): Promise<void> {
         this.params = params
+    }
+}
+
+export class LoadBookListSpy implements LoadBookList {
+    userId: string
+    result = [
+        mockBookModel(),
+        mockBookModel(),
+    ]
+
+    async load (userId: string): Promise<LoadBookList.Result> {
+        this.userId = userId
+        return this.result
     }
 }
