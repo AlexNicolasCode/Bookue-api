54d50d663231c770864d36a077f9fd2b12486aa4
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-24 13:07:18 -0300

    feat: load books interfaces should require only the userId directly, not inside an object

--- src/data/protocols/db/book/load.book.repository.ts
+++ src/data/protocols/db/book/load.book.repository.ts
@@ -1,12 +1,9 @@
 import { BookModel } from "@/domain/models";
 
 export interface LoadBookRepository {
-    load: (bookData: LoadBookRepository.Params) => Promise<LoadBookRepository.Result>;
+    load: (userId: string) => Promise<LoadBookRepository.Result>;
 };
 
 export namespace LoadBookRepository {
-    export type Result = {
-        userId: string
-    }
-    export type Params = BookModel[]
+    export type Result = BookModel[]
 }
