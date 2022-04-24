b2fa4091e584d7d96fc8c74646d4ff8b5c818558
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-24 12:42:50 -0300

    feat: mockAddBookParams was created

--- tests/domain/mocks/mock.book.ts
+++ tests/domain/mocks/mock.book.ts
@@ -1,4 +1,5 @@
 import { BookModel } from "@/domain/models";
+import { AddBook } from "@/domain/usecases";
 
 import faker from '@faker-js/faker';
 
@@ -9,5 +10,16 @@ export const mockBookModel = (): BookModel => {
         description: faker.random.words(),
         currentPage: faker.datatype.number(),
         pages: faker.datatype.number(),
+    };
+};
+
+export const mockAddBookParams = (): AddBook.Params => {
+    return {
+        title: faker.random.words(),
+        author: faker.random.word(),
+        description: faker.random.words(),
+        currentPage: faker.datatype.number(),
+        pages: faker.datatype.number(),
+        userId: faker.datatype.uuid(),
     };
 };
