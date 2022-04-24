2dc11e67eeb1188d62b9b8876b7e8a50b8bf2259
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-24 14:18:09 -0300

    feat: DbAddBook should receive userId on add method too

--- src/data/usecases/db.add.book.ts
+++ src/data/usecases/db.add.book.ts
@@ -1,11 +1,10 @@
-import { BookModel } from "@/domain/models";
 import { AddBook } from "@/domain/usecases";
 import { AddBookRepository } from "../protocols";
 
 export class DbAddBook implements AddBook {
     constructor (private readonly addBookRepository: AddBookRepository) {}
 
-    async add (bookData: BookModel): Promise<void> {
+    async add (bookData: AddBookRepository.Params): Promise<void> {
         await this.addBookRepository.add(bookData)
     };
 }
