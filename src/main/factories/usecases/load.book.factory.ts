5c841b3fcb7b2e825db9d64cd251daf6f53cc8ee
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-24 16:10:26 -0300

    feat: makeDbLoadBook factory was created

---
+++ src/main/factories/usecases/load.book.factory.ts
@@ -0,0 +1,8 @@
+import { DbLoadBook } from "@/data/usecases";
+import { LoadBook } from "@/domain/usecases";
+import { BookMongoRepository } from "@/infra";
+
+export const makeDbLoadBook = (): LoadBook => {
+    const bookMongoRepository = new BookMongoRepository()
+    return new DbLoadBook(bookMongoRepository)
+}
