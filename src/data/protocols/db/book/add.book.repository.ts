cf90744f63b73be024a8ce519e8734d9a006cc62
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-24 14:13:25 -0300

    feat: AddBookRepository params should have userId

--- src/data/protocols/db/book/add.book.repository.ts
+++ src/data/protocols/db/book/add.book.repository.ts
@@ -1,9 +1,14 @@
-import { BookModel } from "@/domain/models";
-
 export interface AddBookRepository {
     add: (bookData: AddBookRepository.Params) => Promise<void>;
 };
 
 export namespace AddBookRepository {
-    export type Params = BookModel
+    export type Params = {
+        title: string
+        author: string
+        description: string
+        currentPage: number
+        pages: number
+        userId: string
+    }
 }
