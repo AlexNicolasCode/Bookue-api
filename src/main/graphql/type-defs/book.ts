c9f75febc1d76fa0d278475529939ce64f00675c
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-24 15:08:36 -0300

    feat: loadAllBooks type def was added to graphql system

--- src/main/graphql/type-defs/book.ts
+++ src/main/graphql/type-defs/book.ts
@@ -1,6 +1,10 @@
 import { gql } from "apollo-server-express";
 
 export default gql`
+    type Query {
+        loadAllBooks (userId: String!): [Book]
+    }
+
     type Mutation {
         addBook (title: String!, author: String!, description: String, currentPage: String, pages: String!, userId: String!): Book!
     }
