c40a68891141945e7945b6b2fd8a44027e157a42
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-24 16:30:23 -0300

    feat: loadOneBook method was added to graphql type defs

--- src/main/graphql/type-defs/book.ts
+++ src/main/graphql/type-defs/book.ts
@@ -3,6 +3,7 @@ import { gql } from "apollo-server-express";
 export default gql`
     type Query {
         loadAllBooks (userId: String!): [Book]
+        loadOneBook (userId: String!, bookId: String!): Book
     }
 
     type Mutation {
