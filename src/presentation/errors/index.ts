710e5741c51ac9baec0e79231a70d97c83622853
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-24 12:21:40 -0300

    feat: InvalidPageCountError was created

--- src/presentation/errors/index.ts
+++ src/presentation/errors/index.ts
@@ -3,3 +3,4 @@ export * from './server.error';
 export * from './email.already.used';
 export * from './invalid.param.error';
 export * from './unauthorized.error';
+export * from './invalid.page.count.error';
