188eff98732989b84cc29ede8b5221243c01cb40
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-24 13:14:07 -0300

    feat: LoadBooksRepository was mocked

--- tests/data/mocks/mock.book.repository.ts
+++ tests/data/mocks/mock.book.repository.ts
@@ -1,10 +1,26 @@
-import { AddBookRepository } from "@/data/protocols";
+import { AddBookRepository, LoadBooksRepository } from "@/data/protocols";
 import { BookModel } from "@/domain/models";
+import { mockBookModel } from "tests/domain/mocks";
 
 export class AddBookRepositorySpy implements AddBookRepository {
     bookData: AddBookRepository.Params
 
     async add (bookData: BookModel): Promise<void> {
         this.bookData = bookData
+    }
+}
+
+export class LoadBooksRepositorySpy implements LoadBooksRepository {
+    userId: string
+    result = [
+        mockBookModel(),
+        mockBookModel(),
+        mockBookModel(),
+        mockBookModel(),
+    ]
+
+    async load (userId: string): Promise<LoadBooksRepository.Result> {
+        this.userId = userId
+        return this.result
     }
 }
