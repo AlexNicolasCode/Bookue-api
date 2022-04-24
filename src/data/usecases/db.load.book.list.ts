6c53ff6765a6a0a47d3cc2a2c5328f4323c9bd71
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-24 13:40:24 -0300

    refactor: update class name DbLoadBooks to DbLoadBookList to improve code readability

--- src/data/usecases/db.load.book.list.ts
+++ src/data/usecases/db.load.book.list.ts
@@ -1,7 +1,7 @@
 import { LoadBookList } from "@/domain/usecases";
 import { LoadBookListRepository } from "../protocols";
 
-export class DbLoadBooks implements LoadBookList {
+export class DbLoadBookList implements LoadBookList {
     constructor (private readonly loadBookListRepository: LoadBookListRepository) {}
 
     async load (userId: string): Promise<LoadBookList.Result> {
