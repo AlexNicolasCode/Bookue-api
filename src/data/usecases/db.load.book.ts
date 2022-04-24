11b51e311691349aaca28d3cfd85ae3c448ba608
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-24 15:41:33 -0300

    feat: DbLoadBook was created

---
+++ src/data/usecases/db.load.book.ts
@@ -0,0 +1,10 @@
+import { LoadBook } from "@/domain/usecases";
+import { LoadBookRepository } from "../protocols";
+
+export class DbLoadBook implements LoadBook {
+    constructor (private readonly loadBookRepository: LoadBookRepository) {}
+
+    async load (data: LoadBook.Request): Promise<LoadBook.Result> {
+        return await this.loadBookRepository.load(data)
+    }
+}
