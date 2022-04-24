895a11bc48bdc497d46ebab01d043adc6cd67974
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-24 12:34:19 -0300

    refactor: remove useless construtor

--- src/validation/validators/pages.count.validation.ts
+++ src/validation/validators/pages.count.validation.ts
@@ -2,8 +2,6 @@ import { InvalidPageCountError } from "@/presentation/errors";
 import { Validation } from "@/presentation/protocols";
 
 export class PageCountValidation implements Validation {
-    constructor () {}
-
     validate (input: any): Error {
         if (input.pages < input.currentPage) {
             return new InvalidPageCountError()
