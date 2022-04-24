655f917bf69eeb3d98d4fb868c8f761fa9c20c78
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-24 14:14:59 -0300

    feat: BookMongoRepository should receive userId on add method too

--- src/infra/db/book.mongo.repository.ts
+++ src/infra/db/book.mongo.repository.ts
@@ -1,9 +1,8 @@
 import { AddBookRepository, LoadBookListRepository } from "@/data/protocols";
-import { BookModel } from "@/domain/models";
 import { MongoHelper } from "./mongo.helper";
 
 export class BookMongoRepository implements AddBookRepository, LoadBookListRepository {
-    async add (bookData: BookModel): Promise<void> {
+    async add (bookData: AddBookRepository.Params): Promise<void> {
         try {
             await MongoHelper.addOneOn('book', bookData);
         } catch (e) {
