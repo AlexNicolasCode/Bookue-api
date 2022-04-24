c14053f6f1ec399dc326ac1b648c173a48f6337f
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-24 15:08:24 -0300

    feat: loadAllBooks resolver was added to graphql system

--- src/main/graphql/resolvers/book.ts
+++ src/main/graphql/resolvers/book.ts
@@ -1,7 +1,11 @@
 import { adaptResolver } from "@/main/adapters";
-import { makeAddBookController } from "@/main/factories/controllers";
+import { makeAddBookController, makeLoadBookListController } from "@/main/factories/controllers";
 
 export default {
+    Query: {
+        loadAllBooks: async (parent: any, args: any) => adaptResolver(makeLoadBookListController(), args)
+    },
+
     Mutation: {
         addBook: async (parent: any, args: any, context: any) => adaptResolver(makeAddBookController(), args, context)
     }
