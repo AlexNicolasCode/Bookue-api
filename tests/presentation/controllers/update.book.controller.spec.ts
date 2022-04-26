e5324b5b6ddd2d5e22b660598a3e54baf7c72190
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-26 14:34:24 -0300

    feat: UpdateBookController should return 500 if Validation throws

---
+++ tests/presentation/controllers/update.book.controller.spec.ts
@@ -0,0 +1,35 @@
+import { badRequest, serverError } from "@/presentation/helpers"
+import { Controller, HttpResponse, Validation } from "@/presentation/protocols"
+import { mockUpdateBookRequest } from "tests/domain/mocks"
+import { throwError } from "tests/domain/mocks/test.helpers"
+import { ValidationSpy } from "../mocks"
+
+export class UpdateBookController implements Controller {
+    constructor (
+        private readonly validation: Validation,
+    ) {}
+
+    async handle (request: any): Promise<HttpResponse> {
+        try {
+            const error = await this.validation.validate(request)
+            if (error) {
+                return badRequest(error)
+            }
+        } catch (error) {
+            return serverError(error)
+        }
+    }
+}
+
+describe('UpdateBookController', () => {
+    test('should return 500 if Validation throws', async () => {
+        const validationSpy = new ValidationSpy()
+        const sut = new UpdateBookController(validationSpy)
+        const fakeRequest = mockUpdateBookRequest() 
+        jest.spyOn(validationSpy, 'validate').mockImplementationOnce(throwError)
+
+        const httpResponse = await sut.handle(fakeRequest)
+
+        expect(httpResponse.statusCode).toBe(500)
+    })
+})
