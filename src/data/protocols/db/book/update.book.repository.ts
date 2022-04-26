0a9e87a2906c675f0dff7dedb407b065cbd9fad8
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-26 13:26:25 -0300

    refactor: UpdateBookRepository should require bookId to update book data

--- src/data/protocols/db/book/update.book.repository.ts
+++ src/data/protocols/db/book/update.book.repository.ts
@@ -4,11 +4,12 @@ export interface UpdateBookRepository {
 
 export namespace UpdateBookRepository {
     export type Params = {
-        title: string
-        author: string
-        description: string
-        currentPage: number
-        pages: number
+        title?: string
+        author?: string
+        description?: string
+        currentPage?: number
+        pages?: number
         userId: string
+        bookId: string
     }
 }
