0c442d1ffb112a7f04b2da3a87dce752a8b8751c
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-26 13:21:00 -0300

    feat: UpdateBookRepository was created

---
+++ src/data/protocols/db/book/update.book.repository.ts
@@ -0,0 +1,14 @@
+export interface UpdateBookRepository {
+    update: (bookData: UpdateBookRepository.Params) => Promise<void>;
+};
+
+export namespace UpdateBookRepository {
+    export type Params = {
+        title: string
+        author: string
+        description: string
+        currentPage: number
+        pages: number
+        userId: string
+    }
+}
