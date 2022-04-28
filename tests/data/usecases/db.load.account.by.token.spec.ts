c00d3e9ccaaed9e6b0c97268263cc20fa20fe890
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-28 14:22:51 -0300

    test: DbLoadAccountByToken should return null if LoadAccountByTokenRepository throws

--- tests/data/usecases/db.load.account.by.token.spec.ts
+++ tests/data/usecases/db.load.account.by.token.spec.ts
@@ -1,30 +1,48 @@
-import { Decrypter } from "@/data/protocols";
+import { Decrypter, LoadAccountByTokenRepository } from "@/data/protocols";
 import { LoadAccountByToken } from "@/domain/usecases";
 import faker from "@faker-js/faker";
 import { throwError } from "tests/domain/mocks/test.helpers";
-import { DecrypterSpy } from "../mocks";
+import { DecrypterSpy, LoadAccountByTokenRepositorySpy } from "../mocks";
 
 export class DbLoadAccountByToken implements LoadAccountByToken {
     constructor (
-        private readonly decrypter: Decrypter
+        private readonly decrypter: Decrypter,
+        private readonly loadAccountByTokenRepository: LoadAccountByTokenRepository,
     ) {}
 
     async load (accessToken: string, role?: string): Promise<LoadAccountByToken.Result> {
+        let token: string
         try {
-            await this.decrypter.decrypt(accessToken)
+            token = await this.decrypter.decrypt(accessToken)
+            if (token) {
+                const account = await this.loadAccountByTokenRepository.loadByToken(accessToken, role)
+                return account ?? null
+            }
         } catch (error) {
             return null
         }
-        return
     }
 }
 
 describe('DbLoadAccountByToken', () => {
-    test('should return null if decrypt throws', async () => {
+    test('should return null if Decrypt throws', async () => {
         const decrypterSpy = new DecrypterSpy()
-        const sut = new DbLoadAccountByToken(decrypterSpy)
+        const loadAccountByTokenRepositorySpy = new LoadAccountByTokenRepositorySpy()
+        const sut = new DbLoadAccountByToken(decrypterSpy, loadAccountByTokenRepositorySpy)
         const fakeAccessToken = faker.internet.password()
         jest.spyOn(decrypterSpy, 'decrypt').mockImplementationOnce(throwError)
+
+        const result = await sut.load(fakeAccessToken)
+
+        expect(result).toBeNull()
+    })
+
+    test('should return null if LoadAccountByTokenRepository throws', async () => {
+        const decrypterSpy = new DecrypterSpy()
+        const loadAccountByTokenRepositorySpy = new LoadAccountByTokenRepositorySpy()
+        const sut = new DbLoadAccountByToken(decrypterSpy, loadAccountByTokenRepositorySpy)
+        const fakeAccessToken = faker.internet.password()
+        jest.spyOn(loadAccountByTokenRepositorySpy, 'loadByToken').mockImplementationOnce(throwError)
 
         const result = await sut.load(fakeAccessToken)
 
