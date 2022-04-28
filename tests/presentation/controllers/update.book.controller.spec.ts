9d4d4be18dedfcff69d2868981d7a93d60e2578b
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-28 13:08:44 -0300

    refactor: tests with same http response was to gather to avoid test duplication

--- tests/presentation/controllers/update.book.controller.spec.ts
+++ tests/presentation/controllers/update.book.controller.spec.ts
@@ -31,15 +31,6 @@ describe('UpdateBookController', () => {
         const httpResponse = await sut.handle(fakeRequest)
 
         expect(httpResponse.statusCode).toBe(500)
-    })
-
-    test('should return Error on body if Validation throws', async () => {
-        const { sut, validationSpy, } = makeSut()
-        const fakeRequest = mockUpdateBookRequest() 
-        jest.spyOn(validationSpy, 'validate').mockImplementationOnce(throwError)
-
-        const httpResponse = await sut.handle(fakeRequest)
-
         expect(httpResponse.body).toStrictEqual(serverError(new Error()).body)
     })
 
@@ -51,15 +42,6 @@ describe('UpdateBookController', () => {
         const httpResponse = await sut.handle(fakeRequest)
 
         expect(httpResponse.statusCode).toStrictEqual(400)
-    })
-
-    test('should return Error on body if Validation returns error', async () => {
-        const { sut, validationSpy, } = makeSut()
-        const fakeRequest = mockUpdateBookRequest() 
-        validationSpy.error = new Error()
-
-        const httpResponse = await sut.handle(fakeRequest)
-
         expect(httpResponse.body).toStrictEqual(new Error())
     })
 
@@ -71,33 +53,16 @@ describe('UpdateBookController', () => {
         const httpResponse = await sut.handle(fakeRequest)
 
         expect(httpResponse.statusCode).toStrictEqual(500)
-    })
-
-    test('should return ServerError on body if UpdateBook throws', async () => {
-        const { sut, updateBookSpy, } = makeSut()
-        const fakeRequest = mockUpdateBookRequest() 
-        jest.spyOn(updateBookSpy, 'update').mockImplementationOnce(throwError)
-
-        const httpResponse = await sut.handle(fakeRequest)
-
         expect(httpResponse.body).toStrictEqual(new ServerError(new Error().stack))
     })
 
-    test('should return 204 on status code on success', async () => {
+    test('should return 204 on success', async () => {
         const { sut } = makeSut()
         const fakeRequest = mockUpdateBookRequest() 
 
         const httpResponse = await sut.handle(fakeRequest)
 
         expect(httpResponse.statusCode).toStrictEqual(204)
-    })
-
-    test('should return null on body on success', async () => {
-        const { sut } = makeSut()
-        const fakeRequest = mockUpdateBookRequest() 
-
-        const httpResponse = await sut.handle(fakeRequest)
-
         expect(httpResponse.body).toBeNull()
     })
 })
