86fbd7b8879c61d4db102c0f2da78a903d7eddba
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-29 13:42:46 -0300

    test: DbDeleteBook should throws if DeleteBookRepository throws

--- tests/data/usecases/db.delete.book.spec.ts
+++ tests/data/usecases/db.delete.book.spec.ts
@@ -1,15 +1,19 @@
 import { DeleteBook } from "@/domain/usecases"
-import { CheckAccountByAccessTokenRepository } from "@/data/protocols"
-import { CheckAccountByAccessTokenRepositorySpy } from "../mocks"
+import { CheckAccountByAccessTokenRepository, DeleteBookRepository } from "@/data/protocols"
+import { CheckAccountByAccessTokenRepositorySpy, DeleteBookRepositorySpy } from "../mocks"
 import { mockDeleteBookRequest } from "tests/domain/mocks"
 import { throwError } from "tests/domain/mocks/test.helpers"
 
 export class DbDeleteBook implements DeleteBook {
-    constructor (private readonly checkAccountByAccessTokenRepository: CheckAccountByAccessTokenRepository) {}
+    constructor (
+        private readonly checkAccountByAccessTokenRepository: CheckAccountByAccessTokenRepository,
+        private readonly deleteBookRepository: DeleteBookRepository,
+    ) {}
 
     async delete (data: DeleteBook.Params): Promise<boolean> {
         const hasAccount = await this.checkAccountByAccessTokenRepository.checkByAccessToken(data.accessToken)
         if (hasAccount) {
+            await this.deleteBookRepository.delete(data)
             return true
         }
     }
@@ -18,9 +22,22 @@ export class DbDeleteBook implements DeleteBook {
 describe('DbDeleteBook', () => {
     test('should throws if CheckAccountByAccessTokenRepository throws', async () => {
         const checkAccountByAccessTokenRepositorySpy = new CheckAccountByAccessTokenRepositorySpy()
-        const sut = new DbDeleteBook(checkAccountByAccessTokenRepositorySpy)
+        const deleteBookRepositorySpy = new DeleteBookRepositorySpy()
+        const sut = new DbDeleteBook(checkAccountByAccessTokenRepositorySpy, deleteBookRepositorySpy)
         const fakeRequest = mockDeleteBookRequest()
         jest.spyOn(checkAccountByAccessTokenRepositorySpy, 'checkByAccessToken').mockImplementationOnce(throwError)
+        
+        const promise = sut.delete(fakeRequest)
+        
+        expect(promise).rejects.toThrowError()
+    })
+
+    test('should throws if DeleteBookRepository throws', async () => {
+        const checkAccountByAccessTokenRepositorySpy = new CheckAccountByAccessTokenRepositorySpy()
+        const deleteBookRepositorySpy = new DeleteBookRepositorySpy()
+        const sut = new DbDeleteBook(checkAccountByAccessTokenRepositorySpy, deleteBookRepositorySpy)
+        const fakeRequest = mockDeleteBookRequest()
+        jest.spyOn(deleteBookRepositorySpy, 'delete').mockImplementationOnce(throwError)
 
         const promise = sut.delete(fakeRequest)
 
