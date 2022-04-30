6da08f3adf7edb38a48b7b9774796cc0edd2212c
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-30 17:55:56 -0300

    test: DbAddNote should throw if CheckAccountByAccessTokenRepository throws

--- tests/data/usecases/db.add.note.spec.ts
+++ tests/data/usecases/db.add.note.spec.ts
@@ -1,27 +1,45 @@
 import { AddNote } from "@/domain/usecases"
-import { AddNoteRepository } from "@/data/protocols"
-import { AddNoteRepositorySpy } from "../mocks"
+import { AddNoteRepository, CheckAccountByAccessTokenRepository } from "@/data/protocols"
+import { AddNoteRepositorySpy, CheckAccountByAccessTokenRepositorySpy } from "../mocks"
 import { mockNoteModel } from "tests/domain/mocks"
 import { throwError } from "tests/domain/mocks/test.helpers"
 
 export class DbAddNote implements AddNote {
-    constructor (private readonly addNoteRepository: AddNoteRepository) {}
+    constructor (
+        private readonly checkAccountByAccessTokenRepository: CheckAccountByAccessTokenRepository,
+        private readonly addNoteRepository: AddNoteRepository,
+    ) {}
 
-    async add (data: AddNoteRepository.Params): Promise<void> {
-        await this.addNoteRepository.add(data)
+    async add (data: AddNoteRepository.Params): Promise<boolean> {
+        const hasAccount = await this.checkAccountByAccessTokenRepository.checkByAccessToken(data.accessToken)
+        if (hasAccount) {
+            return await this.addNoteRepository.add(data)          
+        } 
     }
 }
 
 describe('DbAddNote', () => {
     test('should throw if AddNoteRepository throws', async () => {
+        const checkAccountByAccessTokenRepositorySpy = new CheckAccountByAccessTokenRepositorySpy()
         const addNoteRepositorySpy = new AddNoteRepositorySpy()
-        const sut = new DbAddNote(addNoteRepositorySpy)
+        const sut = new DbAddNote(checkAccountByAccessTokenRepositorySpy, addNoteRepositorySpy)
         const fakeNote = mockNoteModel()
         jest.spyOn(addNoteRepositorySpy, 'add').mockImplementationOnce(throwError)
 
         const promise = sut.add(fakeNote)
 
-        expect(addNoteRepositorySpy.add).toBeCalledTimes(1)
-        expect(promise).rejects.toThrowError()
+        await expect(promise).rejects.toThrowError()
+    })
+
+    test('should throw if CheckAccountByAccessTokenRepository throws', async () => {
+        const checkAccountByAccessTokenRepositorySpy = new CheckAccountByAccessTokenRepositorySpy()
+        const addNoteRepositorySpy = new AddNoteRepositorySpy()
+        const sut = new DbAddNote(checkAccountByAccessTokenRepositorySpy, addNoteRepositorySpy)
+        const fakeNote = mockNoteModel()
+        jest.spyOn(checkAccountByAccessTokenRepositorySpy, 'checkByAccessToken').mockImplementationOnce(throwError)
+
+        const promise = sut.add(fakeNote)
+
+        await expect(promise).rejects.toThrowError()
     })
 })
