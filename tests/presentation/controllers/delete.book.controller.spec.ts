f2ad3aa3a87b2f845291bef21ad9aa7ed0a62cfa
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-30 16:16:39 -0300

    test: DeleteBookController should return 403 if DeleteBook return false

--- tests/presentation/controllers/delete.book.controller.spec.ts
+++ tests/presentation/controllers/delete.book.controller.spec.ts
@@ -1,18 +1,25 @@
 import { DeleteBook } from "@/domain/usecases";
-import { badRequest, serverError } from "@/presentation/helpers";
+import { AccessDeniedError } from "@/presentation/errors";
+import { badRequest, forbidden, ok, serverError } from "@/presentation/helpers";
 import { Controller, HttpResponse, Validation } from "@/presentation/protocols";
 import { mockDeleteBookRequest } from "tests/domain/mocks";
-import { throwError } from "tests/domain/mocks/test.helpers";
-import { ValidationSpy } from "../mocks";
+import { DeleteBookSpy, ValidationSpy } from "../mocks";
 
 export class DeleteBookController implements Controller {
-    constructor (private readonly validation: Validation) {}
+    constructor (
+        private readonly validation: Validation,
+        private readonly deleteBook: DeleteBook,
+    ) {}
     
     async handle (request: DeleteBook.Params): Promise<HttpResponse> {
         try {
             const error = this.validation.validate(request)
             if (error) {
                 return badRequest(error)
+            }
+            const isValid = await this.deleteBook.delete(request)
+            if (!isValid) {
+                return forbidden(new AccessDeniedError())
             }
         } catch (error) {
             return serverError(error)
@@ -24,7 +31,8 @@ export class DeleteBookController implements Controller {
 describe('DeleteBookController', () => {
     test('should return 400 if Validation return error', async () => {
         const validationSpy = new ValidationSpy()
-        const sut = new DeleteBookController(validationSpy)
+        const deleteBookSpy = new DeleteBookSpy()
+        const sut = new DeleteBookController(validationSpy, deleteBookSpy)
         const fakeRequest = mockDeleteBookRequest()
         jest.spyOn(validationSpy, 'validate').mockReturnValueOnce(new Error())
 
@@ -32,5 +40,18 @@ describe('DeleteBookController', () => {
 
         expect(httpResponse.statusCode).toStrictEqual(400)
         expect(httpResponse.body).toStrictEqual(new Error())
+    })
+
+    test('should return 403 if DeleteBook return false', async () => {
+        const validationSpy = new ValidationSpy()
+        const deleteBookSpy = new DeleteBookSpy()
+        const sut = new DeleteBookController(validationSpy, deleteBookSpy)
+        const fakeRequest = mockDeleteBookRequest()
+        jest.spyOn(deleteBookSpy, 'delete').mockResolvedValueOnce(false)
+
+        const httpResponse = await sut.handle(fakeRequest)
+
+        expect(httpResponse.statusCode).toStrictEqual(403)
+        expect(httpResponse.body).toStrictEqual(new AccessDeniedError())
     })
 })
