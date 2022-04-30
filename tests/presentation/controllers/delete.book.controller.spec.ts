03bf9f5bf41f3b1a7cf3c39407b5aef8b7282a2d
Alex Nicolas <alexoliveira604@gmail.com>
2022-04-30 16:23:50 -0300

    feat: DeleteBookController was created

--- tests/presentation/controllers/delete.book.controller.spec.ts
+++ tests/presentation/controllers/delete.book.controller.spec.ts
@@ -1,33 +1,8 @@
-import { DeleteBook } from "@/domain/usecases";
+import { DeleteBookController } from "@/presentation/controllers";
 import { AccessDeniedError, ServerError } from "@/presentation/errors";
-import { badRequest, forbidden, noContent, serverError } from "@/presentation/helpers";
-import { Controller, HttpResponse, Validation } from "@/presentation/protocols";
 import { mockDeleteBookRequest } from "tests/domain/mocks";
 import { throwError } from "tests/domain/mocks/test.helpers";
 import { DeleteBookSpy, ValidationSpy } from "../mocks";
-
-export class DeleteBookController implements Controller {
-    constructor (
-        private readonly validation: Validation,
-        private readonly deleteBook: DeleteBook,
-    ) {}
-    
-    async handle (request: DeleteBook.Params): Promise<HttpResponse> {
-        try {
-            const error = this.validation.validate(request)
-            if (error) {
-                return badRequest(error)
-            }
-            const isValid = await this.deleteBook.delete(request)
-            if (!isValid) {
-                return forbidden(new AccessDeniedError())
-            }
-            return noContent()
-        } catch (error) {
-            return serverError(error)
-        } 
-    }
-}
 
 type SutType = {
     sut: DeleteBookController
