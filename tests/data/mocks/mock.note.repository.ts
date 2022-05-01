3781c7d95fc9abb0ca7c3bf0d88ebefb0103182e
Alex Nicolas <alexoliveira604@gmail.com>
2022-05-01 01:06:07 -0300

    feat: LoadNotesRepository was mocked

--- tests/data/mocks/mock.note.repository.ts
+++ tests/data/mocks/mock.note.repository.ts
@@ -1,9 +1,21 @@
-import { AddNoteRepository } from "@/data/protocols"
+import { AddNoteRepository, LoadNotesRepository } from "@/data/protocols"
+import { NoteResultModel } from "@/domain/models"
+import { mockLoadNotes } from "tests/domain/mocks"
 
 export class AddNoteRepositorySpy implements AddNoteRepository {
     params: AddNoteRepository.Params
 
     async add (params: AddNoteRepository.Params): Promise<void> {
         this.params = params
+    }
+}
+
+export class LoadNotesRepositorySpy implements LoadNotesRepository {
+    params: LoadNotesRepository.Params
+    result = mockLoadNotes()
+
+    async loadAll (params: LoadNotesRepository.Params): Promise<NoteResultModel[]> {
+        this.params = params
+        return this.result
     }
 }
