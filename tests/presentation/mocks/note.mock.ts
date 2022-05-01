5b6859f6f6256c2ef99abfedf699ca691f50e5b5
Alex Nicolas <alexoliveira604@gmail.com>
2022-05-01 01:59:12 -0300

    feat: LoadNotes was mocked

--- tests/presentation/mocks/note.mock.ts
+++ tests/presentation/mocks/note.mock.ts
@@ -1,10 +1,21 @@
-import { AddNote } from "@/domain/usecases"
+import { AddNote, LoadNotes } from "@/domain/usecases"
+import { mockLoadNotes } from "tests/domain/mocks"
 
 export class AddNoteSpy implements AddNote {
     params: AddNote.Params
     result = true
 
     async add (params: AddNote.Params): Promise<boolean> {
+        this.params = params
+        return this.result
+    }
+}
+
+export class LoadNotesSpy implements LoadNotes {
+    params: LoadNotes.Params
+    result = mockLoadNotes()
+
+    async loadAll (params: LoadNotes.Params): Promise<LoadNotes.Result> {
         this.params = params
         return this.result
     }
