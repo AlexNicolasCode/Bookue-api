10ca724f63cd79cbe2379a4c2b25a4265fcbaae0
Alex Nicolas <alexoliveira604@gmail.com>
2022-05-01 01:19:14 -0300

    feat: DbLoadNotes was created

--- tests/data/usecases/db.load.notes.spec.ts
+++ tests/data/usecases/db.load.notes.spec.ts
@@ -1,22 +1,9 @@
-import { CheckAccountByAccessTokenRepository, LoadNotesRepository } from "@/data/protocols";
-import { NoteResultModel } from "@/domain/models";
-import faker from "@faker-js/faker";
+import { LoadNotesRepository } from "@/data/protocols";
 import { throwError } from "tests/domain/mocks/test.helpers";
 import { CheckAccountByAccessTokenRepositorySpy, LoadNotesRepositorySpy } from "../mocks";
+import { DbLoadNotes } from "@/data/usecases";
 
-export class DbLoadNotes implements LoadNotesRepository {
-    constructor (
-        private readonly checkAccountByAccessToken: CheckAccountByAccessTokenRepository,
-        private readonly loadNotesRepository: LoadNotesRepository,
-    ) {}
-
-    async loadAll (data: LoadNotesRepository.Params): Promise<NoteResultModel[]> {
-        const hasAccount = await this.checkAccountByAccessToken.checkByAccessToken(data.accessToken)
-        if (hasAccount) {
-            return this.loadNotesRepository.loadAll(data)
-        }
-    }
-}
+import faker from "@faker-js/faker";
 
 const mockRequest = (): LoadNotesRepository.Params => ({
     accessToken: faker.datatype.uuid(),
