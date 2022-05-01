cc4cedce4ce01f4182ec7d7fd9f94cdd31979294
Alex Nicolas <alexoliveira604@gmail.com>
2022-05-01 01:31:32 -0300

    refactor: an describe should be added for each method added on NoteMongoRepository

--- tests/infra/db/note.mongo.repository.spec.ts
+++ tests/infra/db/note.mongo.repository.spec.ts
@@ -7,22 +7,24 @@ const makeSut = (): NoteMongoRepository => {
 }
 
 describe('NoteMongoRepository', () => {
-    test('should throw if MongoHelper throws', async () => {
-        const sut = makeSut()
-        const fakeData = mockNoteModel()
-        jest.spyOn(MongoHelper, 'addNote').mockImplementationOnce(throwError)
-
-        const promise = sut.add(fakeData)
-
-        await expect(promise).rejects.toThrowError()
-    })
-
-    test('should return undefined on success', async () => {
-        const sut = makeSut()
-        const fakeData = mockNoteModel()
-
-        const result = await sut.add(fakeData)
-
-        expect(result).toBeUndefined()
+    describe('add()', () => {
+        test('should throw if MongoHelper throws', async () => {
+            const sut = makeSut()
+            const fakeData = mockNoteModel()
+            jest.spyOn(MongoHelper, 'addNote').mockImplementationOnce(throwError)
+    
+            const promise = sut.add(fakeData)
+    
+            await expect(promise).rejects.toThrowError()
+        })
+    
+        test('should return undefined on success', async () => {
+            const sut = makeSut()
+            const fakeData = mockNoteModel()
+    
+            const result = await sut.add(fakeData)
+    
+            expect(result).toBeUndefined()
+        })
     })
 })
