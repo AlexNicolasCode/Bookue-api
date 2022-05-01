e61a03d19c018c623620d9a6a64342877cf67aa2
Alex Nicolas <alexoliveira604@gmail.com>
2022-05-01 01:36:23 -0300

    test: NoteMongoRepository should throw if MongoHelper throws on loadAll method

--- src/infra/db/note.mongo.repository.ts
+++ src/infra/db/note.mongo.repository.ts
@@ -1,11 +1,19 @@
-import { AddNoteRepository } from "@/data/protocols"
+import { AddNoteRepository, LoadNotesRepository } from "@/data/protocols"
 import { NoteModel } from "@/domain/models"
 import { MongoHelper } from "./mongo.helper"
 
-export class NoteMongoRepository implements AddNoteRepository {
+export class NoteMongoRepository implements AddNoteRepository, LoadNotesRepository {
     async add (data: NoteModel): Promise<void> {
         try {
             await MongoHelper.addNote(data)
+        } catch (error) {
+            throw new Error(error)
+        }
+    }
+
+    async loadAll (data: LoadNotesRepository.Params): Promise<LoadNotesRepository.Result> {
+        try {
+            return await MongoHelper.loadNotes(data)
         } catch (error) {
             throw new Error(error)
         }
