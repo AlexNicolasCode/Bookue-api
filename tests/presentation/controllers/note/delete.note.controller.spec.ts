d932457a864a0578e317b56413297cb574f8f698
Alex Nicolas <alexoliveira604@gmail.com>
2022-05-04 22:59:00 -0300

    test: DeleteNoteController should return 403 if DeleteNote returns false

--- tests/presentation/controllers/note/delete.note.controller.spec.ts
+++ tests/presentation/controllers/note/delete.note.controller.spec.ts
@@ -1,6 +1,6 @@
 import { DeleteNote } from "@/domain/usecases";
 import { AccessDeniedError } from "@/presentation/errors";
-import { badRequest, serverError } from "@/presentation/helpers";
+import { badRequest, forbidden, serverError } from "@/presentation/helpers";
 import { Controller, HttpResponse, Validation } from "@/presentation/protocols";
 import { mockLoadNotesParams } from "tests/domain/mocks";
 import { throwError } from "tests/domain/mocks/test.helpers";
@@ -20,7 +20,7 @@ export class DeleteNoteController implements Controller {
             }
             const isDeleted = await this.deleteNote.delete(request)
             if (!isDeleted) {
-                return
+                return forbidden(new AccessDeniedError())
             }
         } catch (error) {
             return serverError(error)
@@ -84,5 +84,16 @@ describe('DeleteNoteController', () => {
 
         expect(httpReponse.statusCode).toBe(500)
         expect(httpReponse.body).toStrictEqual(serverError(new Error).body)
+    })
+
+    test('should return 403 if DeleteNote returns false', async () => {
+        const { sut, deleteNoteSpy } = makeSut()
+        const fakeRequest = mockLoadNotesParams()
+        deleteNoteSpy.result = false
+
+        const httpReponse = await sut.handle(fakeRequest)
+
+        expect(httpReponse.statusCode).toBe(403)
+        expect(httpReponse.body).toStrictEqual(new AccessDeniedError())
     })
 })
