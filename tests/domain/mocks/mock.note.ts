61702f5770b8a5ee455f93f4cdf593e40dbfc229
Alex Nicolas <alexoliveira604@gmail.com>
2022-05-05 01:17:28 -0300

    fix: delete book interfaces should have noteId was params

--- tests/domain/mocks/mock.note.ts
+++ tests/domain/mocks/mock.note.ts
@@ -1,4 +1,4 @@
-import { LoadNotesRepository } from "@/data/protocols"
+import { DeleteNoteRepository, LoadNotesRepository } from "@/data/protocols"
 import { NoteModel, NoteResultModel } from "@/domain/models"
 
 import faker from "@faker-js/faker"
@@ -27,4 +27,10 @@ export const mockLoadNotes = (): NoteResultModel[] => ([
 export const mockLoadNotesParams = (): LoadNotesRepository.Params => ({
     accessToken: faker.datatype.uuid(),
     bookId: faker.datatype.uuid(),
+})
+
+export const mockDeleteNotesParams = (): DeleteNoteRepository.Params => ({
+    accessToken: faker.datatype.uuid(),
+    bookId: faker.datatype.uuid(),
+    noteId: faker.datatype.uuid(),
 })
