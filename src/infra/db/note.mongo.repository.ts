128343217f7865fa1612e47a9fcf1397a3ac2cc5
Alex Nicolas <alexoliveira604@gmail.com>
2022-05-12 00:04:00 -0300

    refactor: NoteMongoRepository should use mongoose lib on logic to add new note

--- src/infra/db/note.mongo.repository.ts
+++ src/infra/db/note.mongo.repository.ts
@@ -1,11 +1,18 @@
 import { AddNoteRepository, LoadNotesRepository } from "@/data/protocols"
 import { NoteModel } from "@/domain/models"
 import { MongoHelper } from "./mongo.helper"
+import { Note, User } from "./mongoose.schemas"
 
 export class NoteMongoRepository implements AddNoteRepository, LoadNotesRepository {
     async add (data: NoteModel): Promise<void> {
         try {
-            await MongoHelper.addNote(data)
+            const account = await User.findOne({ accessToken: data.accessToken })
+            await Note.create({
+                userId: account.id,
+                bookId: data.bookID,
+                text: data.text,
+                createdAt: new Date()
+            })
         } catch (error) {
             throw new Error(error)
         }
