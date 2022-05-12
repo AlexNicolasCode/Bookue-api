a1584244b08fb67fffb4d865ab93ce96e238a289
Alex Nicolas <alexoliveira604@gmail.com>
2022-05-11 23:30:26 -0300

    refactor: removed useless error handling

--- src/infra/db/book.mongo.repository.ts
+++ src/infra/db/book.mongo.repository.ts
@@ -10,49 +10,29 @@ import { Book, User } from "./mongoose.schemas"
 
 export class BookMongoRepository implements AddBookRepository, LoadBooksRepository, LoadBookRepository, UpdateBookRepository, DeleteBookRepository {
     async add (data: AddBookRepository.Params): Promise<void> {
-        try {
-            const account = await User.findOne({ accessToken: data.accessToken })
-            await Book.create({ ...data, userId: account.id })
-        } catch (error) {
-            throw new Error(error)
-        }
+        const account = await User.findOne({ accessToken: data.accessToken })
+        await Book.create({ ...data, userId: account.id })
     }
 
     async loadAll (accessToken: string): Promise<LoadBooksRepository.Result> {
-        try {
-            const account = await User.findOne({ accessToken: accessToken }) as UserModel
-            const books = await Book.find({ userId: account.id })
-            return books
-        } catch (error) {
-            throw new Error(error)
-        }
+        const account = await User.findOne({ accessToken: accessToken }) as UserModel
+        const books = await Book.find({ userId: account.id })
+        return books
     }
 
     async loadOne (data: LoadBookRepository.Request): Promise<BookModel> {
-        try {
-            const account = await User.findOne({ accessToken: data.accessToken }) as UserModel
-            const book = await Book.findOne({ userId: account.id, bookId: data.bookId })
-            return book
-        } catch (error) {
-            throw new Error(error)
-        }
+        const account = await User.findOne({ accessToken: data.accessToken }) as UserModel
+        const book = await Book.findOne({ userId: account.id, bookId: data.bookId })
+        return book
     }
 
     async update (data: UpdateBookRepository.Params): Promise<void> {
-        try {
-            const account = await User.findOne({ accessToken: data.accessToken }) as UserModel
-            await Book.updateOne({ userId: account.id, bookId: data.bookId })
-        } catch (error) {
-            throw new Error(error)
-        }
+        const account = await User.findOne({ accessToken: data.accessToken }) as UserModel
+        await Book.updateOne({ userId: account.id, bookId: data.bookId })
     }
     
     async delete (data: DeleteBookRepository.Params): Promise<void> {
-        try {
-            const account = await User.findOne({ accessToken: data.accessToken }) as UserModel
-            await Book.deleteOne({ userId: account.id, bookId: data.bookId })
-        } catch (error) {
-            throw new Error(error)
-        }
+        const account = await User.findOne({ accessToken: data.accessToken }) as UserModel
+        await Book.deleteOne({ userId: account.id, bookId: data.bookId })
     }
 }
