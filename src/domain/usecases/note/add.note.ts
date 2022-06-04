721f6f9a9defeaccc946e585319ed34d5c38649e
Alex Nicolas <alexoliveira604@gmail.com>
2022-06-03 22:38:16 -0300

    fix: AddNote was changed to allow receive only correct values as params

--- src/domain/usecases/note/add.note.ts
+++ src/domain/usecases/note/add.note.ts
@@ -1,9 +1,11 @@
-import { NoteModel } from "@/domain/models"
-
 export interface AddNote {
     add: (data: AddNote.Params) => Promise<boolean>
 }
 
 export namespace AddNote {
-    export type Params = NoteModel
+    export type Params = {
+        accessToken: string
+        bookId: string
+        text: string
+    }
 }
