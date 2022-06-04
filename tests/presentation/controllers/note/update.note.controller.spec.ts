90c9bd440a801a2262e6c87f2dda845aa11cdf0f
Alex Nicolas <alexoliveira604@gmail.com>
2022-06-04 15:38:23 -0300

    test: UpdateNoteController should call UpdateNote with correct values

--- tests/presentation/controllers/note/update.note.controller.spec.ts
+++ tests/presentation/controllers/note/update.note.controller.spec.ts
@@ -1,13 +1,14 @@
 import { UpdateNote } from "@/domain/usecases";
 import { Controller, HttpResponse, Validation } from "@/presentation/protocols";
-import { ValidationSpy } from "tests/presentation/mocks";
+import { UpdateNoteSpy, ValidationSpy } from "tests/presentation/mocks";
 import { badRequest } from "@/presentation/helpers";
 
 import faker from "@faker-js/faker";
 
 class UpdateNoteController implements Controller {
     constructor (
-        private readonly validation: Validation
+        private readonly validation: Validation,
+        private readonly updateNote: UpdateNote,
     ) {}
 
     async handle (request: UpdateNote.Params): Promise<HttpResponse> {
@@ -15,6 +16,7 @@ class UpdateNoteController implements Controller {
         if (error) {
             return badRequest(error)
         }
+        await this.updateNote.update(request)
         return
     }
 }
@@ -33,7 +35,8 @@ describe('UpdateNoteController', () => {
 
     test('should call Validation with correct values', async () => {
         const validationSpy = new ValidationSpy()
-        const sut = new UpdateNoteController(validationSpy)
+        const updateNoteSpy = new UpdateNoteSpy()
+        const sut = new UpdateNoteController(validationSpy, updateNoteSpy)
 
         await sut.handle(fakeRequest)
 
@@ -42,12 +45,23 @@ describe('UpdateNoteController', () => {
 
     test('should return 400 if validation return error', async () => {
         const validationSpy = new ValidationSpy()
-        const sut = new UpdateNoteController(validationSpy)
+        const updateNoteSpy = new UpdateNoteSpy()
+        const sut = new UpdateNoteController(validationSpy, updateNoteSpy)
         validationSpy.error = new Error()
 
         const httpResponse = await sut.handle(fakeRequest)
 
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toBe(validationSpy.error)
+    })
+
+    test('should call UpdateNote with correct values', async () => {
+        const validationSpy = new ValidationSpy()
+        const updateNoteSpy = new UpdateNoteSpy()
+        const sut = new UpdateNoteController(validationSpy, updateNoteSpy)
+
+        await sut.handle(fakeRequest)
+
+        expect(updateNoteSpy.params).toBe(fakeRequest)
     })
 })
