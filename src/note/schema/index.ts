971007aa5a9643039af85713954878728e0c5a28
Alex Nicolas <alexoliveira604@gmail.com>
2022-06-04 16:05:47 -0300

    refactor: removed useless old files

--- src/note/schema/index.ts
+++
@@ -1,9 +0,0 @@
-import { model, Schema } from "mongoose"
-
-const NoteSchema = new Schema({
-    bookId: string,
-    text: string,
-    createdAt: Date
-})
-
-export const Note = model("notes", NoteSchema)
