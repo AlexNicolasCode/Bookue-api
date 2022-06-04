77970a9406984698ee6e743c693a7f8bb37c0700
Alex Nicolas <alexoliveira604@gmail.com>
2022-06-03 23:49:42 -0300

    feat: DbUpdateNote was created

--- tests/data/usecases/note/db.update.note.spec.ts
+++ tests/data/usecases/note/db.update.note.spec.ts
@@ -1,28 +1,9 @@
-import { LoadAccountByTokenRepository, UpdateNoteRepository } from "@/data/protocols";
 import { UpdateNote } from "@/domain/usecases";
 import { LoadAccountByTokenRepositorySpy, UpdateNoteRepositorySpy } from "tests/data/mocks";
 import { throwError } from "tests/domain/mocks/test.helpers";
+import { DbUpdateNote } from "@/data/usecases";
 
 import faker from "@faker-js/faker";
-
-class DbUpdateNote implements UpdateNote {
-    constructor (
-        private readonly loadAccountByTokenRepository: LoadAccountByTokenRepository,
-        private readonly updateNoteRepository: UpdateNoteRepository,
-    ) {}
-
-    async update (data: UpdateNote.Params): Promise<void> {
-        const account = await this.loadAccountByTokenRepository.loadByToken(data.accessToken)
-        if (account) {
-            await this.updateNoteRepository.update({ 
-                userId: account.id, 
-                bookId: data.bookId, 
-                noteId: data.noteId, 
-                text: data.text, 
-            })
-        }
-    }
-}
 
 type SutTypes = {
     sut: DbUpdateNote
