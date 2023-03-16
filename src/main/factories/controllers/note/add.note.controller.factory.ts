a63f6604a46ccec131727d35b64ccdbb67aa25e2
Alex Nicolas <alexoliveira604@gmail.com>
2023-03-15 23:23:18 -0300

    fix: import name update to allow build

--- src/main/factories/controllers/note/add.note.controller.factory.ts
+++ src/main/factories/controllers/note/add.note.controller.factory.ts
@@ -1,7 +1,7 @@
 import { AddNoteController } from "@/presentation/controllers"
 import { Controller } from "@/presentation/protocols"
 import { makeDbAddNote } from "../../usecases"
-import { makeAddNoteValidation } from "./add.Note.validation.factory"
+import { makeAddNoteValidation } from "./add.note.validation.factory"
 
 export const makeAddNoteController = (): Controller => {
     const controller = new AddNoteController(makeAddNoteValidation(), makeDbAddNote())
