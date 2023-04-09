90964ac662fde8ab1a78405cf64fcd838258b82d
Alex Nicolas <alexoliveira604@gmail.com>
2023-04-08 22:59:45 -0300

    feat: DbDeleteBook should follow SRP from SOLID to preserve architecure planning

--- src/data/usecases/book/db.delete.book.ts
+++ src/data/usecases/book/db.delete.book.ts
@@ -1,18 +1,15 @@
 import { DeleteBook } from "@/domain/usecases"
-import { DeleteBookRepository, LoadAccountByTokenRepository } from "@/data/protocols"
+import { DeleteBookRepository } from "@/data/protocols"
 
 export class DbDeleteBook implements DeleteBook {
     constructor (
-        private readonly loadAccountByTokenRepository: LoadAccountByTokenRepository,
         private readonly deleteBookRepository: DeleteBookRepository,
     ) {}
 
-    async delete (data: DeleteBook.Params): Promise<boolean> {
-        const account = await this.loadAccountByTokenRepository.loadByToken(data.accessToken)
-        if (account) {
-            await this.deleteBookRepository.delete({ userId: account.id, bookId: data.bookId })
-            return true
-        }
-        return false
+    async delete (data: DeleteBook.Params): Promise<void> {
+        await this.deleteBookRepository.delete({
+            userId: data.userId,
+            bookId: data.bookId,
+        })
     }
 }
--- tests/data/usecases/book/db.delete.book.spec.ts
+++ tests/data/usecases/book/db.delete.book.spec.ts
@@ -1,55 +1,23 @@
-import { DeleteBookRepositorySpy, LoadAccountByTokenRepositorySpy } from "../../mocks"
+import { DeleteBookRepositorySpy } from "../../mocks"
 import { mockDeleteBookParams } from "tests/domain/mocks"
 import { throwError } from "tests/domain/mocks/test.helpers"
 import { DbDeleteBook } from "@/data/usecases"
 
 type SutType = {
     sut: DbDeleteBook
-    loadAccountByTokenRepositorySpy: LoadAccountByTokenRepositorySpy
     deleteBookRepositorySpy: DeleteBookRepositorySpy
 }
 
 const makeSut = (): SutType => {
-    const loadAccountByTokenRepositorySpy = new LoadAccountByTokenRepositorySpy()
     const deleteBookRepositorySpy = new DeleteBookRepositorySpy()
-    const sut = new DbDeleteBook(loadAccountByTokenRepositorySpy, deleteBookRepositorySpy)
+    const sut = new DbDeleteBook(deleteBookRepositorySpy)
     return {
         sut,
-        loadAccountByTokenRepositorySpy,
         deleteBookRepositorySpy,
     }
 }
 
 describe('DbDeleteBook', () => {
-    test('should throws if LoadAccountByTokenRepository throws', async () => {
-        const { sut, loadAccountByTokenRepositorySpy } = makeSut()
-        const fakeRequest = mockDeleteBookParams()
-        jest.spyOn(loadAccountByTokenRepositorySpy, 'loadByToken').mockImplementationOnce(throwError)
-        
-        const promise = sut.delete(fakeRequest)
-        
-        expect(promise).rejects.toThrowError()
-    })
-
-    test('should call LoadAccountByTokenRepository with correct values', async () => {
-        const { sut, loadAccountByTokenRepositorySpy } = makeSut()
-        const fakeRequest = mockDeleteBookParams()
-        
-        await sut.delete(fakeRequest)
-        
-        expect(loadAccountByTokenRepositorySpy.token).toBe(fakeRequest.accessToken)
-    })
-
-    test('should return undefined if access token is invalid', async () => {
-        const { sut, loadAccountByTokenRepositorySpy } = makeSut()
-        const fakeRequest = mockDeleteBookParams()
-        jest.spyOn(loadAccountByTokenRepositorySpy, 'loadByToken').mockResolvedValueOnce(undefined)
-        
-        const result = await sut.delete(fakeRequest)
-        
-        expect(result).toBe(false)
-    })
-
     test('should throws if DeleteBookRepository throws', async () => {
         const { sut, deleteBookRepositorySpy } = makeSut()
         const fakeRequest = mockDeleteBookParams()
@@ -61,22 +29,11 @@ describe('DbDeleteBook', () => {
     })
     
     test('should call DeleteBookRepository with correct values', async () => {
-        const { sut, loadAccountByTokenRepositorySpy, deleteBookRepositorySpy } = makeSut()
+        const { sut, deleteBookRepositorySpy } = makeSut()
         const fakeRequest = mockDeleteBookParams()
-        const fakeAccount = loadAccountByTokenRepositorySpy.result
         
         await sut.delete(fakeRequest)
         
-        expect(deleteBookRepositorySpy.userId).toBe(fakeAccount.id)
-        expect(deleteBookRepositorySpy.bookId).toBe(fakeRequest.bookId)
-    })
-    
-    test('should return true on success', async () => {
-        const { sut } = makeSut()
-        const fakeRequest = mockDeleteBookParams()
-        
-        const result = await sut.delete(fakeRequest)
-        
-        expect(result).toBe(true)
+        expect(deleteBookRepositorySpy).toBe(fakeRequest)
     })
 })
