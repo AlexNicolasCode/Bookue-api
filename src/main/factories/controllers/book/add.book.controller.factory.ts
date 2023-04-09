f9c4c2066bfc68603e5890e570ac24468921aa79
Alex Nicolas <alexoliveira604@gmail.com>
2023-04-08 21:53:15 -0300

    feat: add DbLoadAccountByToken as depency in factory to avoid broken application

--- src/main/factories/controllers/book/add.book.controller.factory.ts
+++ src/main/factories/controllers/book/add.book.controller.factory.ts
@@ -1,8 +1,15 @@
 import { AddBookController } from "@/presentation/controllers"
 import { Controller } from "@/presentation/protocols"
-import { makeDbAddBook } from "../../usecases/book"
 import { makeAddBookValidation } from "@/main/factories/validators"
+import { makeDbAddBook, makeDbLoadAccountByToken } from "../../usecases"
 
 export const makeAddBookController = (): Controller => {
-    return new AddBookController(makeAddBookValidation(), makeDbAddBook())
+    const addBookValidation = makeAddBookValidation()
+    const dbAddBook = makeDbAddBook()
+    const dbLoadAccountByToken = makeDbLoadAccountByToken()
+    return new AddBookController(
+        addBookValidation,
+        dbAddBook,
+        dbLoadAccountByToken,
+    )
 }
