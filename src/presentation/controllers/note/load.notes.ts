f199a5a1756a40038d3b0a328380ca94f1089b07
Alex Nicolas <alexoliveira604@gmail.com>
2023-04-13 19:44:10 -0300

    feat: LoadNodesController should return forbidden when user account is not found

--- src/presentation/controllers/note/load.notes.ts
+++ src/presentation/controllers/note/load.notes.ts
@@ -1,4 +1,4 @@
-import { LoadNotes } from "@/domain/usecases"
+import { LoadAccountByToken, LoadNotes } from "@/domain/usecases"
 import { AccessDeniedError } from "@/presentation/errors"
 import { badRequest, forbidden, ok, noContent, serverError } from "@/presentation/helpers"
 import { Controller, Validation, HttpResponse } from "@/presentation/protocols"
@@ -6,22 +6,34 @@ import { Controller, Validation, HttpResponse } from "@/presentation/protocols"
 export class LoadNotesController implements Controller {
     constructor (
         private readonly validation: Validation,
+        private readonly loadAccountByToken: LoadAccountByToken,
         private readonly loadNotes: LoadNotes,
     ) {}
 
-    async handle (request: LoadNotes.Params): Promise<HttpResponse> {
+    async handle (request: LoadNotesController.Request): Promise<HttpResponse> {
         try {
             const error = this.validation.validate(request)
             if (error) {
                 return badRequest(error)
             }
-            const notes = await this.loadNotes.loadAll(request)
-            if (!notes) {
+            const account = await this.loadAccountByToken.load(request.accessToken)
+            if (!account) {
                 return forbidden(new AccessDeniedError())
             }
+            const notes = await this.loadNotes.loadAll({
+                userId: account.id,
+                bookId: request.bookId,
+            })
             return notes.length ? ok(notes) : noContent()
         } catch (error) {
             return serverError(error)
         }
+    }
+}
+
+export namespace LoadNotesController {
+    export type Request = {
+        accessToken: string
+        bookId: string
     }
 }
