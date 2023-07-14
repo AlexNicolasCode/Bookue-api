f968d176408dbe3115de507cfeaa026611c29535
Alex Nicolas <alexoliveira604@gmail.com>
2023-07-14 00:25:13 -0300

    test: change mocked tests to integrations tests

--- tests/infra/db/account.mongo.repository.test.ts
+++ tests/infra/db/account.mongo.repository.test.ts
@@ -4,6 +4,7 @@ import { faker } from "@faker-js/faker"
 
 import { AccountMongoRepository, User } from "@/infra"
 import { mockAddAccountParams, mockAccount } from "tests/domain/mocks"
+import { UserModel } from "@/domain/models"
 
 const makeSut = (): AccountMongoRepository => {
     return new AccountMongoRepository()
@@ -11,6 +12,7 @@ const makeSut = (): AccountMongoRepository => {
 
 describe('AccountMongoRepository', () => {
     let mongoDb: MongoMemoryServer;
+    let fakeRequest: Partial<UserModel>
 
     beforeAll(async () => {
         mongoDb = await MongoMemoryServer.create();
@@ -23,103 +25,118 @@ describe('AccountMongoRepository', () => {
     })
 
     beforeEach(async () => {
+        fakeRequest = {
+            name: faker.name.firstName(),
+            email: faker.internet.email(),
+            password: faker.internet.password(),
+            accessToken: faker.datatype.uuid(),
+        } 
         await User.deleteMany({})
     })
 
-    test('should add one only user', async () => {
-        const sut = makeSut()
-        const fakeAccount = mockAddAccountParams()
-        
-        await sut.add(fakeAccount)
+    describe('add()', () => {
+        test('should add one only user', async () => {
+            const sut = makeSut()
+            const fakeAccount = mockAddAccountParams()
+            
+            await sut.add(fakeAccount)
 
-        const count = await User.countDocuments({ email: fakeAccount.email })
-        expect(count).toBe(1)
+            const count = await User.countDocuments({ email: fakeAccount.email })
+            expect(count).toBe(1)
+        })
     })
 
-    test('should return true when account exists', async () => {
-        const sut = makeSut()
-        const fakeAccount = mockAddAccountParams()
-        await User.create(fakeAccount)      
-          
-        const result = await sut.checkByEmail(fakeAccount.email)
+    describe('checkByEmail()', () => {
+        test('should return true when account exists', async () => {
+            const sut = makeSut()
+            const fakeAccount = mockAddAccountParams()
+            await User.create(fakeAccount)      
+            
+            const result = await sut.checkByEmail(fakeAccount.email)
+            
+            expect(result).toBe(true)
+        })
         
-        expect(result).toBe(true)
-    })
-    
-    test('should return false when account not exists', async () => {
-        const sut = makeSut()
-        const fakeAccount = mockAddAccountParams()
-        
-        const result = await sut.checkByEmail(fakeAccount.email)
-        
-        expect(result).toBe(false)
-    })
-    
-    test('should return data account on success', async () => {
-        const sut = makeSut()
-        const fakeAccount = mockAddAccountParams()
-        await User.create(fakeAccount)
-
-        const result = await sut.loadByEmail(fakeAccount.email)
-        
-        expect({
-            name: result.name,
-            email: result.email,
-            password: result.password,
-        }).toStrictEqual(fakeAccount)
+        test('should return false when account not exists', async () => {
+            const sut = makeSut()
+            const fakeAccount = mockAddAccountParams()
+            
+            const result = await sut.checkByEmail(fakeAccount.email)
+            
+            expect(result).toBe(false)
+        })
     })
     
-    test('should call updateAccessToken to update token on success', async () => {
-        const sut = makeSut()
-        const accountMock = mockAddAccountParams()
-        const fakeToken = faker.datatype.uuid()
-        await User.create(accountMock)
+    describe('loadByEmail()', () => {
+        test('should return data account on success', async () => {
+            const sut = makeSut()
+            const fakeAccount = mockAddAccountParams()
+            await User.create(fakeAccount)
 
-        const account = await User.findOne({ email: accountMock.email })
-        expect(account.accessToken).toBeUndefined()
-        await sut.updateAccessToken(account.id, fakeToken)
+            const result = await sut.loadByEmail(fakeAccount.email)
+            
+            expect({
+                name: result.name,
+                email: result.email,
+                password: result.password,
+            }).toStrictEqual(fakeAccount)
+        })
+    })
+    
+    describe('updateAccessToken()', () => {
+        test('should call updateAccessToken to update token on success', async () => {
+            const sut = makeSut()
+            const accountMock = mockAddAccountParams()
+            const fakeToken = faker.datatype.uuid()
+            await User.create(accountMock)
 
-        const accountAfterUpdateAccessToken = await User.findOne({ email: accountMock.email })
-        expect(accountAfterUpdateAccessToken.accessToken).toBe(fakeToken)
+            const account = await User.findOne({ email: accountMock.email })
+            expect(account.accessToken).toBeUndefined()
+            await sut.updateAccessToken(account.id, fakeToken)
+
+            const accountAfterUpdateAccessToken = await User.findOne({ email: accountMock.email })
+            expect(accountAfterUpdateAccessToken.accessToken).toBe(fakeToken)
+        })
     })
 
-    test('should return true when account exists', async () => {
-        const sut = makeSut()
-        const fakeAccount = mockAccount()
-        jest.spyOn(User, 'findOne').mockResolvedValueOnce(fakeAccount)
-        
-        const result = await sut.checkByAccessToken(fakeAccount._id)
+    describe('checkByAccessToken()', () => {
+        test('should return true when account exists', async () => {
+            const sut = makeSut()
+            const fakeAccount = mockAccount()
+            fakeAccount._id = undefined
+            await User.create(fakeAccount)
+            
+            const result = await sut.checkByAccessToken(fakeAccount.accessToken)
 
-        expect(result).toBe(true)
+            expect(result).toBe(true)
+        })
+
+        test('should return false when account not exists', async () => {
+            const sut = makeSut()
+            
+            const result = await sut.checkByAccessToken(fakeRequest.accessToken)
+
+            expect(result).toBe(false)
+        })
     })
 
-    test('should return false when account exists', async () => {
-        const sut = makeSut()
-        const fakeAccount = mockAccount()
-        jest.spyOn(User, 'findOne').mockResolvedValueOnce(undefined)
-        
-        const result = await sut.checkByAccessToken(fakeAccount._id)
+    describe('loadByToken()', () => {
+        test('should return account id if access token is valid', async () => {
+            const sut = makeSut()
+            const fakeCreatedAcount = await User.create(mockAddAccountParams())
+            
+            const result = await sut.loadByToken(fakeCreatedAcount.accessToken)
 
-        expect(result).toBe(false)
-    })
+            expect(result).toStrictEqual({ id: fakeCreatedAcount.id })
+        })
 
-    test('should return account id if access token is valid', async () => {
-        const sut = makeSut()
-        const fakeAccount = mockAccount()
-        jest.spyOn(User, 'findOne').mockResolvedValueOnce(fakeAccount)
-        
-        const result = await sut.loadByToken(fakeAccount.accessToken)
+        test('should return undefined if User model on findOne method not found account', async () => {
+            const sut = makeSut()
+            const fakeAccount = mockAccount()
+            
+            const result = await sut.loadByToken(fakeAccount.accessToken)
 
-        expect(result).toStrictEqual({ id: fakeAccount._id })
-    })
-
-    test('should return undefined if User model on findOne method not found account', async () => {
-        const sut = makeSut()
-        const fakeAccount = mockAccount()
-        jest.spyOn(User, 'findOne').mockResolvedValueOnce(undefined)
-        
-        const result = await sut.loadByToken(fakeAccount.accessToken)
-
-        expect(result).toBeUndefined()
+            expect(result).toBeUndefined()
+        })
     })
 })
