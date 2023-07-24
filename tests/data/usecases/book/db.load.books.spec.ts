55523138ab43c4e51af65a95c527224f19974be3
Alex Nicolas <alexoliveira604@gmail.com>
2023-07-24 18:23:44 -0300

    fix: removed validation from DbLoadBooks usecases

--- tests/data/usecases/book/db.load.books.spec.ts
+++ tests/data/usecases/book/db.load.books.spec.ts
@@ -1,63 +1,48 @@
 import { DbLoadBooks } from "@/data/usecases"
 import { throwError } from "tests/domain/mocks/test.helpers"
-import { LoadAccountByTokenRepositorySpy, LoadBooksRepositorySpy } from "../../mocks"
-import { mockAccount } from "tests/domain/mocks"
+import { LoadBooksRepositorySpy } from "../../mocks"
 
 import { faker } from "@faker-js/faker"
 
 type SutType = {
     sut: DbLoadBooks
     loadBooksRepositorySpy: LoadBooksRepositorySpy
-    loadAccountByTokenRepositorySpy: LoadAccountByTokenRepositorySpy
 }
 
 const makeSut = (): SutType => {
-    const loadAccountByTokenRepositorySpy = new LoadAccountByTokenRepositorySpy()
     const loadBooksRepositorySpy = new LoadBooksRepositorySpy()
-    const sut = new DbLoadBooks(loadAccountByTokenRepositorySpy, loadBooksRepositorySpy)
+    const sut = new DbLoadBooks(loadBooksRepositorySpy)
     return {
         sut,
         loadBooksRepositorySpy,
-        loadAccountByTokenRepositorySpy,
     }
 }
 
 describe('DbLoadBooks', () => {
     test('should throw if LoadBooksRepository throws', async () => {
         const { sut, loadBooksRepositorySpy } = makeSut()
-        const fakeAccessToken = faker.datatype.uuid()
+        const fakeUserId = faker.datatype.uuid()
         jest.spyOn(loadBooksRepositorySpy, 'loadAll').mockImplementationOnce(throwError)
 
-        const promise = sut.load(fakeAccessToken)
+        const promise = sut.load(fakeUserId)
 
         expect(promise).rejects.toThrowError()
     })
 
     test('should call LoadBooksRepository with correct userId', async () => {
-        const { sut, loadAccountByTokenRepositorySpy, loadBooksRepositorySpy } = makeSut()
-        const fakeAccessToken = faker.datatype.uuid()
-        const fakeUserId = mockAccount()
-        loadAccountByTokenRepositorySpy.result = { id: fakeUserId._id }
+        const { sut, loadBooksRepositorySpy } = makeSut()
+        const fakeUserId = faker.datatype.uuid()
 
-        await sut.load(fakeAccessToken)
+        await sut.load(fakeUserId)
 
-        expect(loadBooksRepositorySpy.userId).toBe(fakeUserId._id)
-    })
-
-    test('should call LoadAccountByTokenRepository with correct accessToken', async () => {
-        const { sut, loadAccountByTokenRepositorySpy } = makeSut()
-        const fakeAccessToken = faker.datatype.uuid()
-
-        await sut.load(fakeAccessToken)
-
-        expect(loadAccountByTokenRepositorySpy.token).toBe(fakeAccessToken)
+        expect(loadBooksRepositorySpy.userId).toBe(fakeUserId)
     })
 
     test('should return book list on success', async () => {
         const { sut, loadBooksRepositorySpy } = makeSut()
-        const fakeAccessToken = faker.datatype.uuid()
+        const fakeUserId = faker.datatype.uuid()
 
-        const books = await sut.load(fakeAccessToken)
+        const books = await sut.load(fakeUserId)
 
         expect(books).toBe(loadBooksRepositorySpy.result)
     })
