c8a35cffdc4fb384dcf53ef853c4bcbee2d0ecdd
Alex Nicolas <alexoliveira604@gmail.com>
2023-07-24 20:54:14 -0300

    chore: remove max scope length in commit to improve developer experience

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,7 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    extends: ['@commitlint/config-conventional']
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'scope-max-length': [0, 'never'],
+    }
 };
